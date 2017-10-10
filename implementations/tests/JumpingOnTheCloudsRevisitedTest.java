b8e6759ba120da6a8466818ed4bc93b3b1e33cd7
Tyler Moses <tmosest@gmail.com>
2017-10-09 21:59:58 -0400

    Solved Jumping On The Clouds Revisited.

---
+++ implementations/tests/JumpingOnTheCloudsRevisitedTest.java
@@ -0,0 +1,16 @@
+package implementations.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import implementation.JumpingOnTheCloudsRevisited;
+
+public class JumpingOnTheCloudsRevisitedTest {
+
+  @Test
+  public void test() {
+    int[] clouds = {0, 0, 1, 0, 0, 1, 1, 0,};
+    int jumpSize = 2;
+    Assert.assertEquals(92, JumpingOnTheCloudsRevisited.energyLeft(clouds, jumpSize));
+  }
+
+}

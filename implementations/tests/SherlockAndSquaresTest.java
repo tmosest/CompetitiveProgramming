c65367bd762e0f34da7b2bbbf5f9abac97d8904a
Tyler Moses <tmosest@gmail.com>
2017-10-10 21:34:54 -0400

    Sherlock and Squares solved.

---
+++ implementations/tests/SherlockAndSquaresTest.java
@@ -0,0 +1,15 @@
+package implementations.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import implementation.SherlockAndSquares;
+
+public class SherlockAndSquaresTest {
+
+  @Test
+  public void testCase0() {
+    Assert.assertEquals(2, SherlockAndSquares.countSquaresInclusive(3, 9));
+    Assert.assertEquals(0, SherlockAndSquares.countSquaresInclusive(17, 24));
+  }
+
+}

3cbb8c25b2d58b1d1c8989432be8704d3f7322b8
Tyler Moses <tmosest@gmail.com>
2017-10-16 18:08:24 -0400

    Organizing Containers Of Balls: only solved one test case.... feeling rusty.

--- hackerrank/implementation/OrganizingContainersOfBalls.java
+++ hackerrank/implementation/OrganizingContainersOfBalls.java
@@ -1,10 +1,53 @@
 package hackerrank.implementation;
 
+import java.util.Scanner;
+
 public class OrganizingContainersOfBalls {
 
-  public static boolean canOrganizeBalls (int queries, int[][] matrix) {
-    
-    return false;
+  public static boolean debugMode = false;
+
+  private static boolean canOrganizeBalls(long[] balls) {
+    boolean canSwapEven = true;
+    long toCheck = balls[0];
+    for (int b = 0; b < balls.length; b++) {
+      if (debugMode) {
+        System.out.println("Ball Count " + b + " : " + balls[b]);
+      }
+      if (balls[b] != toCheck) {
+        canSwapEven = false;
+        // break;
+      }
+    }
+    return canSwapEven;
   }
-  
+
+  private static void printAnswers(String[] answers) {
+    for (int i = 0; i < answers.length; i++)
+      System.out.println(answers[i]);
+  }
+
+  public static String[] solve() {
+    Scanner in = new Scanner(System.in);
+    int tests = in.nextInt();
+    String[] answers = new String[tests];
+    for (int i = 0; i < tests; i++) {
+      int bucketsAndBalls = in.nextInt();
+      if (debugMode) {
+        System.out.println("\nTest Case: " + i);
+        System.out.println("Balls and Buckets Count: " + bucketsAndBalls);
+      }
+      long[] balls = new long[bucketsAndBalls];
+      for (int n = 0; n < bucketsAndBalls * bucketsAndBalls; n++) {
+        balls[n % bucketsAndBalls] += in.nextInt();
+      }
+      answers[i] = (canOrganizeBalls(balls)) ? "Possible" : "Impossible";
+    }
+    in.close();
+    printAnswers(answers);
+    return answers;
+  }
+
+  public static void main(String[] args) {
+    solve();
+  }
 }
--- hackerrank/implementation/tests/OrganizingContainersOfBallsTest.java
+++ hackerrank/implementation/tests/OrganizingContainersOfBallsTest.java
@@ -1,14 +1,73 @@
 package hackerrank.implementation.tests;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import org.junit.Assert;
 import org.junit.Test;
+import hackerrank.implementation.OrganizingContainersOfBalls;
 
 public class OrganizingContainersOfBallsTest {
 
+  static String[] answers;
+
+  static BufferedReader out = null;
+
+  static InputStream testInput;
+
+  static String data;
+  static String filePrefix = "hackerrank/implementation/tests/data/OrganizingContainersOfBalls/";
+
+  private static void testFromFile(int fileNumber) throws NumberFormatException, IOException {
+    StringBuilder inputString = new StringBuilder();
+    inputString.append(filePrefix);
+    inputString.append("input");
+    if (fileNumber < 10)
+      inputString.append("0");
+    inputString.append(fileNumber);
+    inputString.append(".txt");
+    StringBuilder outputString = new StringBuilder();
+    outputString.append(filePrefix);
+    outputString.append("output");
+    if (fileNumber < 10)
+      outputString.append("0");
+    outputString.append(fileNumber);
+    outputString.append(".txt");
+    // Get files
+    File inputFile = new File(inputString.toString());
+    File outputFile = new File(outputString.toString());
+    // Get Answer
+    out = new BufferedReader(new FileReader(inputFile));
+    int size = Integer.parseInt(out.readLine());
+    out = new BufferedReader(new FileReader(outputFile));
+    answers = new String[size];
+    for (int i = 0; i < size; i++)
+      answers[i] = out.readLine();
+    // Set Input Stream from File
+    testInput = new FileInputStream(inputFile);
+    System.setIn(testInput);
+  }
+
   @Test
-  public void test() {
-    int queries = 2;
-    int[][] containerMatrix = {{1, 1}, {1, 1}};
-    
+  public void testCase0() throws UnsupportedEncodingException {
+    data = "2\n2\n1 1\n1 1\n2\n0 2\n1 1";
+    testInput = new ByteArrayInputStream(data.getBytes("UTF-8"));
+    System.setIn(testInput);
+    String[] answers = {"Possible", "Impossible"};
+    OrganizingContainersOfBalls.debugMode = false;
+    Assert.assertArrayEquals(answers, OrganizingContainersOfBalls.solve());
+  }
+
+  @Test
+  public void testCase1() throws NumberFormatException, IOException {
+    testFromFile(1);
+    OrganizingContainersOfBalls.debugMode = true;
+    Assert.assertArrayEquals(answers, OrganizingContainersOfBalls.solve());
   }
 
 }

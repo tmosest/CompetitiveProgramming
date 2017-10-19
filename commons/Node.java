95b8dff3f862afdafa916f2bf8014c6d9c3b044e
Tyler Moses <tmosest@gmail.com>
2017-10-18 23:47:18 -0400

    Implemented a String Stack using a linked list.

---
+++ commons/Node.java
@@ -0,0 +1,15 @@
+package commons;
+/**
+ * This class represents a node of data.
+ * @author tmosest
+ * @param <T> What type of data is this node going to hold.
+ */
+public class Node<T> {
+  public T data;
+  public Node<T> next;
+  
+  public Node() {
+    
+  }
+  
+}

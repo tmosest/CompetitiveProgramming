98ceb69231a0dd646ad814f0a08385926250fe86
Tyler Moses <tmosest@gmail.com>
2017-10-18 22:37:28 -0400

    Big Sorting little bit too slow in Java but same logic works in C++

--- hackerrank/sorting/BigSorting.java
+++ hackerrank/sorting/BigSorting.java
@@ -2,21 +2,31 @@ package hackerrank.sorting;
 
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class BigSorting {
-  
-  public static BigInteger[] solve() {
+
+  public static String[] solve() {
     Scanner in = new Scanner(System.in);
     int size = in.nextInt();
     in.nextLine();
-    BigInteger[] array = new BigInteger[size];
-    for(int i = 0; i < size; i++) {
-      array[i] = new BigInteger(in.nextLine());
+    String[] array = new String[size];
+    for (int i = 0; i < size; i++) {
+      array[i] = in.nextLine();
     }
     in.close();
-    Arrays.sort(array);
-    for(int i = 0; i < size; i++) 
+    Arrays.sort(array, new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        int n = o1.length();
+        int m = o2.length();
+        if (n == m)
+          return o1.compareTo(o2);
+        return (n < m) ? -1 : 1;
+      }
+    });
+    for (int i = 0; i < size; i++)
       System.out.println(array[i]);
     return array;
   }
--- hackerrank/sorting/test/BigSortingTest.java
+++ hackerrank/sorting/test/BigSortingTest.java
@@ -19,9 +19,7 @@ public class BigSortingTest {
   public void test() throws UnsupportedEncodingException {
     data = "6\n" + "31415926535897932384626433832795\n" + "1\n" + "3\n" + "10\n" + "3\n" + "5";
     answer = "1\n" + "3\n" + "3\n" + "5\n" + "10\n" + "31415926535897932384626433832795";
-    BigInteger[] answers =
-        {new BigInteger("1"), new BigInteger("3"), new BigInteger("3"), new BigInteger("5"),
-            new BigInteger("10"), new BigInteger("31415926535897932384626433832795")};
+    String[] answers = {"1", "3", "3", "5", "10", "31415926535897932384626433832795"};
     testInput = new ByteArrayInputStream(data.getBytes("UTF-8"));
     System.setIn(testInput);
     Assert.assertArrayEquals(answers, BigSorting.solve());

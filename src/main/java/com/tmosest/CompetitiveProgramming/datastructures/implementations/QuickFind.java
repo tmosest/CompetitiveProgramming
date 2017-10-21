21e42cdd13a3049dc2e4171d36e62ea90e535176
tmosest <tmosest@gmail.com>
2017-10-21 05:29:40 -0400

    Added Back Quick Union Find.

---
+++ src/main/java/com/tmosest/CompetitiveProgramming/datastructures/implementations/QuickFind.java
@@ -0,0 +1,37 @@
+package com.tmosest.CompetitiveProgramming.datastructures.implementations;
+
+import com.tmosest.CompetitiveProgramming.datastructures.UnionFindADT;
+
+public class QuickFind implements UnionFindADT {
+
+  int[] ids;
+
+  private void handleoutOfBounds(int p, int q) throws IndexOutOfBoundsException {
+    if (p < 0 || p >= ids.length || q < 0 || q >= ids.length)
+      throw new IndexOutOfBoundsException();
+  }
+
+  public QuickFind(int n) {
+    ids = new int[n];
+    for (int i = 0; i < n; i++) {
+      ids[i] = i;
+    }
+  }
+
+  public boolean isConnected(int p, int q) throws IndexOutOfBoundsException {
+    handleoutOfBounds(p, q);
+    return ids[p] == ids[q];
+  }
+
+  public void union(int p, int q) throws IndexOutOfBoundsException {
+    handleoutOfBounds(p, q);
+    int pId = ids[p];
+    int qId = ids[q]; // what was wrong with my other approach it has one less variable.
+    for (int i = 0; i < ids.length; i++) {
+      if (ids[i] == pId) {
+        ids[i] = qId;
+      }
+    }
+  }
+
+}

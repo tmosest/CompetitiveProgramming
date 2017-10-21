c430601aace92979e130a247c0f1077e8e28d4fe
Tyler Moses <tmosest@gmail.com>
2017-10-21 05:29:40 -0400

    Added Back Quick Union Find.

---
+++ src/test/java/com/tmosest/CompetitiveProgramming/datastructures/implementations/QuickFindTest.java
@@ -0,0 +1,46 @@
+package com.tmosest.CompetitiveProgramming.datastructures.implementations;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import com.tmosest.CompetitiveProgramming.datastructures.UnionFindADT;
+
+@DisplayName("Quick Find Union Find")
+class QuickFindTest {
+
+  int size = 10;
+  UnionFindADT uf;
+
+  @BeforeEach
+  public void setUp() {
+    uf = new QuickFind(10);
+  }
+
+  @Test
+  @DisplayName("isConnected")
+  public void testIsConnectedForNewUF() throws IndexOutOfBoundsException {
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        if (i == j) {
+          // Test Reflexive Property.
+          Assertions.assertTrue(uf.isConnected(i, j));
+        } else {
+          // Otherwise shouldn't be connected.
+          Assertions.assertFalse(uf.isConnected(i, j));
+        }
+      }
+    }
+  }
+
+  @Test
+  @DisplayName("Union")
+  public void testUnion() throws IndexOutOfBoundsException {
+    uf.union(0, 1);
+    Assertions.assertTrue(uf.isConnected(0, 1));
+    // Test Transitivity
+    uf.union(1, 2);
+    Assertions.assertTrue(uf.isConnected(0, 2));
+  }
+
+}

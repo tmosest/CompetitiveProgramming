365919c18f47aa0507e67a9b177cc8d0c0ae4360
tmosest <tmosest@gmail.com>
2017-10-21 04:42:07 -0400

    Got J-Unit 5 test cases running as part of maven build.

--- src/test/java/com/tmosest/MvnCompetitiveProgramming/hackerrank/warmup/SolveMeFirstTest.java
+++ src/test/java/com/tmosest/MvnCompetitiveProgramming/hackerrank/warmup/SolveMeFirstTest.java
@@ -1,15 +1,18 @@
 package com.tmosest.MvnCompetitiveProgramming.hackerrank.warmup;
 
 import java.io.UnsupportedEncodingException;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import com.tmosest.MvnCompetitiveProgramming.hackerrank.HackerrankUtil;
 
+@DisplayName("Solved Me First")
 public class SolveMeFirstTest {
 
   String data;
-  
+
   @Test
+  @DisplayName("Test Case: 0")
   public void testCase0() throws UnsupportedEncodingException {
     data = "2\n3";
     HackerrankUtil.setSystemIn(data);

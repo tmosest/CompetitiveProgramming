3d66a8366777031a7970d36f19718ec1d2732ddc
tmosest <tmosest@gmail.com>
2017-11-03 00:19:52 -0400

    Solved Sherlock and the Valid String. Really happy with this file for the most part.

---
+++ src/test/java/com/tmosest/CompetitiveProgramming/hackerrank/strings/SherlockAndTheValidStringTest.java
@@ -0,0 +1,56 @@
+package com.tmosest.CompetitiveProgramming.hackerrank.strings;
+
+import java.io.UnsupportedEncodingException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import com.tmosest.CompetitiveProgramming.hackerrank.HackerrankUtil;
+
+@DisplayName("Hackerrank Strings: Sherlock and the Valid String")
+class SherlockAndTheValidStringTest {
+
+  String input;
+  String output;
+
+  /**
+   * Utility Function to actually test the class. Sets the input stream to the input data.
+   * 
+   * @param input
+   * @param output
+   * @param debugMode
+   * @throws UnsupportedEncodingException
+   */
+  private static void test(String input, String output, boolean debugMode)
+      throws UnsupportedEncodingException {
+    HackerrankUtil.setSystemIn(input);
+    SherlockAndTheValidString.debugMode = debugMode;
+    Assertions.assertEquals(output, SherlockAndTheValidString.solve());
+  }
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() throws UnsupportedEncodingException {
+    input = "aabbcd";
+    output = "NO";
+    test(input, output, false);
+  }
+
+  @Test
+  @DisplayName("Test Case 2")
+  void testCase2() throws UnsupportedEncodingException {
+    input =
+        "hfchdkkbfifgbgebfaahijchgeeeiagkadjfcbekbdaifchkjfejckbiiihegacfbchdihkgbkbddgaefhkdgccjejjaajgijdkd";
+    output = "YES";
+    test(input, output, false);
+  }
+
+  @Test
+  @DisplayName("Test Case 5")
+  void testCase5() throws UnsupportedEncodingException {
+    input =
+        "ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd";
+    output = "YES";
+    test(input, output, false);
+  }
+
+}

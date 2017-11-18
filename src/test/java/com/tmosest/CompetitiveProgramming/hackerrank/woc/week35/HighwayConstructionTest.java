1498fa2257edd8815b06aef7308a8b2c9e048598
Tyler Moses <tmosest@gmail.com>
2017-11-18 01:22:25 -0500

    Crazy maths not good enough.

--- src/test/java/com/tmosest/CompetitiveProgramming/hackerrank/woc/week35/HighwayConstructionTest.java
+++ src/test/java/com/tmosest/CompetitiveProgramming/hackerrank/woc/week35/HighwayConstructionTest.java
@@ -5,6 +5,7 @@ import java.math.BigInteger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import com.tmosest.CompetitiveProgramming.hackerrank.HackerrankUtil;
+import com.tmosest.CompetitiveProgramming.hackerrank.woc.week35.HighwayConstruction.Rational;
 
 class HighwayConstructionTest {
 
@@ -18,7 +19,7 @@ class HighwayConstructionTest {
    * @param debugMode
    * @throws UnsupportedEncodingException
    */
-  private static void test(String input, BigInteger[] output, boolean debugMode)
+  private static void test(String input, Rational[] output, boolean debugMode)
       throws UnsupportedEncodingException {
     HackerrankUtil.setSystemIn(input);
     HighwayConstruction.debugMode = debugMode;
@@ -27,8 +28,8 @@ class HighwayConstructionTest {
 
   @Test
   void testCase0() throws UnsupportedEncodingException {
-    input = "1\n4 2";
-    BigInteger[] output = {new BigInteger("13")};
+    input = "1\n4 4";
+    Rational[] output = {new Rational(13,1)};
     test(input, output, true);
   }
 

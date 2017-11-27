6c82ec55c2a0e5640195ff6b8d0f17c846c75502
tmosest <tmosest@gmail.com>
2017-11-26 21:36:26 -0500

    Solved First Data Structure problem.

---
+++ src/main/java/com/tmosest/CompetitiveProgramming/hackerearth/datastructures/arrays/oneD/Tutorial.java
@@ -0,0 +1,33 @@
+package com.tmosest.CompetitiveProgramming.hackerearth.datastructures.arrays.oneD;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Tutorial {
+  
+  public static boolean debugMode = false;
+  
+  public static int[] reverse(int[] arr) {
+    int[] reverse = new int[arr.length];
+    for(int i = 0; i < arr.length; i++) {
+      reverse[i] = arr[arr.length - i - 1];
+    }
+    return reverse;
+  }
+  
+  public static int[] solve() {
+    Scanner in = new Scanner(System.in);
+    int size = in.nextInt();
+    int[] arr = new int[size];
+    for(int i = 0; i < size; i++) {
+      arr[i] = in.nextInt();
+    }
+    in.close();
+    return reverse(arr);
+  }
+  
+  public static void main(String[] args) {
+    Arrays.stream(solve()).forEach(System.out::println);
+  }
+  
+}

e8431593059da40187505c527fd3e99e700d4948
tmosest <tmosest@gmail.com>
2017-12-16 00:41:28 -0500

    Animal Transport one test case

---
+++ src/test/java/com/tmosest/CompetitiveProgramming/hackerrank/wcs/wcs12/AnimalTransportTest.java
@@ -0,0 +1,40 @@
+package com.tmosest.CompetitiveProgramming.hackerrank.wcs.wcs12;
+
+import java.io.UnsupportedEncodingException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import com.tmosest.CompetitiveProgramming.hackerrank.HackerrankUtil;
+
+@DisplayName("Hackerrank WCS World Code Sprint 12: Animal Transport")
+class AnimalTransportTest {
+
+  /**
+   * Utility Function to actually test the class. Sets the input stream to the input data.
+   * 
+   * @param input
+   * @param output
+   * @param debugMode
+   * @throws UnsupportedEncodingException
+   */
+  private static void test(String input, String output, boolean debugMode)
+      throws UnsupportedEncodingException {
+    HackerrankUtil.setSystemIn(input);
+    AnimalTransport.debugMode = debugMode;
+    java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();
+    System.setOut(new java.io.PrintStream(out));
+    AnimalTransport.solve();
+    Assertions.assertEquals(output, out.toString());
+  }
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() throws UnsupportedEncodingException {
+    String input = "2\n" + "10 3\n" + "E D C\n" + "4 1 4\n" + "7 5 8\n" + "10 6\n" + "E D C M E D\n"
+        + "1 1 1 2 9 7\n" + "2 2 2 4 10 10\n";
+    String output = "5 8 -1 \n" + 
+        "2 2 4 10 -1 -1 \n";
+    test(input, output, false);
+  }
+
+}

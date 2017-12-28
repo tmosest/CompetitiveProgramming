374239174644c6b3de4b11dc5579043cf7836b57
Tyler Moses <tmosest@gmail.com>
2017-12-28 16:19:44 -0500

    HackerrankUtil, linted

--- src/main/java/com/tmosest/competitiveprogramming/hackerrank/HackerrankUtil.java
+++ src/main/java/com/tmosest/competitiveprogramming/hackerrank/HackerrankUtil.java
@@ -7,9 +7,8 @@ import java.io.UnsupportedEncodingException;
 
 /**
  * Utility Function to help with Testing.
- * 
- * @author tmosest (Tyler Moses)
  *
+ * @author tmosest (Tyler Moses)
  */
 public class HackerrankUtil {
 
@@ -18,15 +17,16 @@ public class HackerrankUtil {
   static InputStream testInput;
 
   static String data;
-  
+
   /**
    * Sets the data for the System.in for Scanner tests.
-   * @param data
-   * @throws UnsupportedEncodingException
+   *
+   * @param data Information to be shown output to System.in.
+   * @throws UnsupportedEncodingException Error for unsupported content.
    */
   public static void setSystemIn(String data) throws UnsupportedEncodingException {
     testInput = new ByteArrayInputStream(data.getBytes("UTF-8"));
     System.setIn(testInput);
   }
-  
+
 }

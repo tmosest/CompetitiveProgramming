baf04c50bc63b470f777a2ac7f594e1059f75fc3
tmosest <tmosest@gmail.com>
2017-12-28 16:17:47 -0500

    Knight L On A Chessboard, linted

--- src/main/java/com/tmosest/competitiveprogramming/hackerrank/search/KnightLOnAChessboard.java
+++ src/main/java/com/tmosest/competitiveprogramming/hackerrank/search/KnightLOnAChessboard.java
@@ -10,83 +10,82 @@ public class KnightLOnAChessboard {
 
   private static int size;
 
-  private static class Node {
-    public int x;
-    public int y;
-    public int t;
-
-    public Node(int x, int y, int t) {
-      this.x = x;
-      this.y = y;
-      this.t = t;
-    }
-  }
-
-  private static int bfs(int i, int j) {
+  private static int bfs(int row, int column) {
     Queue<Node> queue = new LinkedList<Node>();
     queue.add(new Node(0, 0, 0));
-    if (queue.size() == 0)
+    if (queue.size() == 0) {
       return 0;
+    }
     boolean[][] visited = new boolean[size][size];
     while (queue.size() != 0) {
-      Node n = queue.remove();
-      int x = n.x;
-      int y = n.y;
-      int t = n.t;
-      if (x - i >= 0) {
-        if (y - j >= 0) {
-          addPoint(x - i, y - j, t + 1, queue, visited);
+      Node node = queue.remove();
+      int xcord = node.xcord;
+      int ycord = node.ycord;
+      int time = node.time;
+      if (xcord - row >= 0) {
+        if (ycord - column >= 0) {
+          addPoint(xcord - row, ycord - column, time + 1, queue, visited);
         }
-        if (y + j < size) {
-          addPoint(x - i, y + i, t + 1, queue, visited);
-        }
-      }
-      if (x + i < size) {
-        if (y - j >= 0) {
-          addPoint(x + i, y - j, t + 1, queue, visited);
-        }
-        if (y + j < size) {
-          if (x + i == size - 1 && y + j == size - 1)
-            return t + 1;
-          addPoint(x + i, y + j, t + 1, queue, visited);
+        if (ycord + row < size) {
+          addPoint(xcord - row, ycord + row, time + 1, queue, visited);
         }
       }
-      if (x - j >= 0) {
-        if (y - i >= 0) {
-          addPoint(x - j, y - i, t + 1, queue, visited);
+      if (xcord + row < size) {
+        if (ycord - column >= 0) {
+          addPoint(xcord + row, ycord - column, time + 1, queue, visited);
         }
-        if (y + i < size) {
-          addPoint(x - j, y + i, t + 1, queue, visited);
+        if (ycord + column < size) {
+          if (xcord + row == size - 1 && ycord + column == size - 1) {
+            return time + 1;
+          }
+          addPoint(xcord + row, ycord + column, time + 1, queue, visited);
         }
       }
-      if (x + j < size) {
-        if (y - i >= 0) {
-          addPoint(x + j, y - i, t + 1, queue, visited);
+      if (xcord - column >= 0) {
+        if (ycord - row >= 0) {
+          addPoint(xcord - column, ycord - row, time + 1, queue, visited);
         }
-        if (y + i < size) {
-          if (x + j == size - 1 && y + i == size - 1)
-            return t + 1;
-          addPoint(x + j, y + i, t + 1, queue, visited);
+        if (ycord + row < size) {
+          addPoint(xcord - column, ycord + row, time + 1, queue, visited);
+        }
+      }
+      if (xcord + column < size) {
+        if (ycord - row >= 0) {
+          addPoint(xcord + column, ycord - row, time + 1, queue, visited);
+        }
+        if (ycord + row < size) {
+          if (xcord + column == size - 1 && ycord + row == size - 1) {
+            return time + 1;
+          }
+          addPoint(xcord + column, ycord + row, time + 1, queue, visited);
         }
       }
     }
     return -1;
   }
 
-  private static void addPoint(int x, int y, int t, Queue<Node> queue, boolean[][] visited) {
-    if (visited[x][y])
+  private static void addPoint(int xcord, int ycord, int time, Queue<Node> queue,
+      boolean[][] visited) {
+    if (visited[xcord][ycord]) {
       return;
-    queue.add(new Node(x, y, t));
-    visited[x][y] = true;
+    }
+    queue.add(new Node(xcord, ycord, time));
+    visited[xcord][ycord] = true;
   }
 
+  /**
+   * Function to take in inputs and return possible knight moves.
+   *
+   * @return Return matrix of possible moves.
+   */
   public static int[][] solve() {
     Scanner in = new Scanner(System.in);
     size = in.nextInt();
     in.close();
 
-    if (debugMode)
+    if (debugMode) {
       System.out.println("Looking at board size: " + size);
+    }
 
     int[][] movesBoard = new int[size - 1][size - 1];
 
@@ -99,16 +98,17 @@ public class KnightLOnAChessboard {
         }
       }
     }
-    
-    if(debugMode)
+
+    if (debugMode) {
       printMoves(movesBoard);
-    
+    }
+
     return movesBoard;
   }
-  
+
   private static void printMoves(int[][] matrix) {
-    for(int i = 1; i < size; i++) {
-      for(int j = 1; j < size; j++) {
+    for (int i = 1; i < size; i++) {
+      for (int j = 1; j < size; j++) {
         System.out.print(matrix[i - 1][j - 1] + " ");
       }
       System.out.println();
@@ -117,6 +117,19 @@ public class KnightLOnAChessboard {
 
   public static void main(String[] args) {
     printMoves(solve());
+  }
+
+  private static class Node {
+
+    public int xcord;
+    public int ycord;
+    public int time;
+
+    public Node(int xcord, int ycord, int time) {
+      this.xcord = xcord;
+      this.ycord = ycord;
+      this.time = time;
+    }
   }
 
 }

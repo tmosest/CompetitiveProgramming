891d5f077d6860a54d8003320334670201a172d4
tmosest <tmosest@gmail.com>
2017-12-27 19:04:02 -0500

    Fixed style

--- src/main/java/com/tmosest/CompetitiveProgramming/hackerearth/graphs/bfs/MonkAndTheIslands.java
+++ src/main/java/com/tmosest/CompetitiveProgramming/hackerearth/graphs/bfs/MonkAndTheIslands.java
@@ -1,101 +1,101 @@
 package com.tmosest.CompetitiveProgramming.hackerearth.graphs.bfs;
 
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class MonkAndTheIslands {
 
-    public static boolean debugMode = false;
+  public static boolean debugMode = false;
 
-    private static class Graph {
-        private int V;
-        private int E;
-        private ArrayList<Integer>[] adj;
+  private static class Graph {
+    private int V;
+    private int E;
+    private ArrayList<Integer>[] adj;
 
-        public Graph(int V) {
-            this.V = V;
-            this.E = 0;
-            adj = (ArrayList<Integer>[]) new ArrayList[V];
-            for(int i = 0; i < V; i++)
-                adj[i] = new ArrayList<Integer>();
-        }
-
-        public int V() {
-            return V;
-        }
-
-        public void addEdge(int v, int w) {
-            E++;
-            adj[v].add(w);
-            adj[w].add(v);
-        }
-
-        public Iterable<Integer> adj(int v) {
-            return adj[v];
-        }
+    public Graph(int V) {
+      this.V = V;
+      this.E = 0;
+      adj = (ArrayList<Integer>[]) new ArrayList[V];
+      for (int i = 0; i < V; i++)
+        adj[i] = new ArrayList<Integer>();
     }
 
-    private static class BreadthFirstPaths {
-        private static final int INFINITY = Integer.MAX_VALUE;
-        private boolean[] marked;
-        private int[] edgeTo;
-        private int[] distTo;
-
-        public BreadthFirstPaths(Graph G, int s) {
-            marked = new boolean[G.V()];
-            edgeTo = new int[G.V()];
-            distTo = new int[G.V()];
-            for (int v = 0; v < G.V(); v++)
-                distTo[v] = INFINITY;
-            bfs(G, s);
-        }
-
-        private void bfs(Graph G, int s) {
-            Queue<Integer> q = new LinkedList<Integer>();
-            for(int v = 0; v < G.V(); v++)
-                distTo[v] = INFINITY;
-            distTo[s] = 0;
-            marked[s] = true;
-            q.add(s);
-            while(!q.isEmpty()) {
-                int v = q.poll();
-                for(int w: G.adj(v)) {
-                    if(!marked[w]) {
-                        edgeTo[w] = v;
-                        distTo[w] = distTo[v] + 1;
-                        marked[w] = true;
-                        q.add(w);
-                    }
-                }
-            }
-        }
-
-        public int distTo(int v) {
-            return distTo[v];
-        }
+    public int V() {
+      return V;
     }
 
-    public static int[] solve() {
-        Scanner in = new Scanner(System.in);
-        int tests = in.nextInt();
-        int[] results = new int[tests];
-        for (int t = 0; t < tests; t++) {
-            int nodes = in.nextInt();
-            int edges = in.nextInt();
-            Graph g = new Graph(nodes);
-            for(int e = 0; e < edges; e++)
-                g.addEdge(in.nextInt() - 1, in.nextInt() - 1);
-            BreadthFirstPaths bfs = new BreadthFirstPaths(g, 0);
-            results[t] = bfs.distTo(nodes - 1);
-        }
-        in.close();
-        return results;
+    public void addEdge(int v, int w) {
+      E++;
+      adj[v].add(w);
+      adj[w].add(v);
     }
 
-    public static void main(String[] args) {
-        Arrays.stream(solve()).forEach(System.out::println);
+    public Iterable<Integer> adj(int v) {
+      return adj[v];
     }
+  }
+
+  private static class BreadthFirstPaths {
+    private static final int INFINITY = Integer.MAX_VALUE;
+    private boolean[] marked;
+    private int[] edgeTo;
+    private int[] distTo;
+
+    public BreadthFirstPaths(Graph G, int s) {
+      marked = new boolean[G.V()];
+      edgeTo = new int[G.V()];
+      distTo = new int[G.V()];
+      for (int v = 0; v < G.V(); v++)
+        distTo[v] = INFINITY;
+      bfs(G, s);
+    }
+
+    private void bfs(Graph G, int s) {
+      Queue<Integer> q = new LinkedList<Integer>();
+      for (int v = 0; v < G.V(); v++)
+        distTo[v] = INFINITY;
+      distTo[s] = 0;
+      marked[s] = true;
+      q.add(s);
+      while (!q.isEmpty()) {
+        int v = q.poll();
+        for (int w : G.adj(v)) {
+          if (!marked[w]) {
+            edgeTo[w] = v;
+            distTo[w] = distTo[v] + 1;
+            marked[w] = true;
+            q.add(w);
+          }
+        }
+      }
+    }
+
+    public int distTo(int v) {
+      return distTo[v];
+    }
+  }
+
+  public static int[] solve() {
+    Scanner in = new Scanner(System.in);
+    int tests = in.nextInt();
+    int[] results = new int[tests];
+    for (int t = 0; t < tests; t++) {
+      int nodes = in.nextInt();
+      int edges = in.nextInt();
+      Graph g = new Graph(nodes);
+      for (int e = 0; e < edges; e++)
+        g.addEdge(in.nextInt() - 1, in.nextInt() - 1);
+      BreadthFirstPaths bfs = new BreadthFirstPaths(g, 0);
+      results[t] = bfs.distTo(nodes - 1);
+    }
+    in.close();
+    return results;
+  }
+
+  public static void main(String[] args) {
+    Arrays.stream(solve()).forEach(System.out::println);
+  }
 }
--- src/test/java/com/tmosest/CompetitiveProgramming/hackerearth/graphs/bfs/LevelNodesTest.java
+++ src/test/java/com/tmosest/CompetitiveProgramming/hackerearth/graphs/bfs/LevelNodesTest.java
@@ -1,47 +1,27 @@
 package com.tmosest.CompetitiveProgramming.hackerearth.graphs.bfs;
 
-import com.tmosest.CompetitiveProgramming.hackerrank.HackerrankUtil;
-
+import java.io.UnsupportedEncodingException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.UnsupportedEncodingException;
+import com.tmosest.CompetitiveProgramming.hackerrank.HackerrankUtil;
 
 @DisplayName("HackerEarth Algorithms Graphs BFS: Level Nodes")
 public class LevelNodesTest {
 
-    private static void test(String input, int output, boolean debugMode)
-            throws UnsupportedEncodingException {
-        HackerrankUtil.setSystemIn(input);
-        LevelNodes.debugMode = debugMode;
-        Assertions.assertEquals(output, LevelNodes.solve());
-    }
+  private static void test(String input, int output, boolean debugMode)
+      throws UnsupportedEncodingException {
+    HackerrankUtil.setSystemIn(input);
+    LevelNodes.debugMode = debugMode;
+    Assertions.assertEquals(output, LevelNodes.solve());
+  }
 
-    @Test
-    void testCase0() throws UnsupportedEncodingException {
-        String input = "20\n" +
-                "11 1\n" +
-                "1 2\n" +
-                "13 3\n" +
-                "15 4\n" +
-                "17 5\n" +
-                "11 6\n" +
-                "2 7\n" +
-                "1 8\n" +
-                "15 9\n" +
-                "4 10\n" +
-                "15 12\n" +
-                "5 13\n" +
-                "2 14\n" +
-                "17 15\n" +
-                "15 16\n" +
-                "11 17\n" +
-                "15 18\n" +
-                "9 19\n" +
-                "16 20\n" +
-                "2";
-        int output = 3;
-        test(input, output, false);
-    }
+  @Test
+  void testCase0() throws UnsupportedEncodingException {
+    String input = "20\n" + "11 1\n" + "1 2\n" + "13 3\n" + "15 4\n" + "17 5\n" + "11 6\n" + "2 7\n"
+        + "1 8\n" + "15 9\n" + "4 10\n" + "15 12\n" + "5 13\n" + "2 14\n" + "17 15\n" + "15 16\n"
+        + "11 17\n" + "15 18\n" + "9 19\n" + "16 20\n" + "2";
+    int output = 3;
+    test(input, output, false);
+  }
 }

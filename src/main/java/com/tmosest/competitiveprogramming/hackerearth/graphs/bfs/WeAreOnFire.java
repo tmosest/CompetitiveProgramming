6d11b0896b9f482cc6f44d1cd4ea2f8332599419
tmosest <tmosest@gmail.com>
2017-12-27 22:07:41 -0500

    We Are On Fire, partially solved. Reader might be too slow.

---
+++ src/main/java/com/tmosest/competitiveprogramming/hackerearth/graphs/bfs/WeAreOnFire.java
@@ -0,0 +1,184 @@
+package com.tmosest.competitiveprogramming.hackerearth.graphs.bfs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class WeAreOnFire {
+
+  public static boolean debugMode = false;
+
+  public static void main(String[] args) {
+    Arrays.stream(solve()).forEach(System.out::println);
+  }
+
+  public static int[] solve() {
+    Scanner in = new Scanner(System.in);
+    int rows = in.nextInt();
+    int columns = in.nextInt();
+    int fireballs = in.nextInt();
+    int[] results = new int[fireballs];
+    int[][] matrix = new int[rows][columns];
+    int cityCount = 0;
+    for (int r = 0; r < rows; r++) {
+      for (int c = 0; c < columns; c++) {
+        matrix[r][c] = in.nextInt();
+        if (matrix[r][c] == 1) {
+          ++cityCount;
+        }
+      }
+    }
+    if (debugMode) {
+      System.out.println("city count: " + cityCount);
+    }
+    Graph graph = new Graph(rows * columns);
+    BreadthFirstSearch bfs = new BreadthFirstSearch(graph);
+    for (int r = 0; r < rows; r++) {
+      for (int c = 0; c < columns; c++) {
+        int from = convertToNode(r, c, rows, columns);
+        if (debugMode) {
+          System.out.println("from: " + from);
+        }
+        if (matrix[r][c] == 1) {
+          int to;
+          if (r > 0 && matrix[r - 1][c] == 1) {
+            to = convertToNode(r - 1, c, rows, columns);
+            if (debugMode) {
+              System.out.println("to: " + to);
+            }
+            graph.addEdge(from, to);
+          }
+          if (r < rows - 1 && matrix[r + 1][c] == 1) {
+            to = convertToNode(r + 1, c, rows, columns);
+            if (debugMode) {
+              System.out.println("to: " + to);
+            }
+            graph.addEdge(from, to);
+          }
+          if (c > 0 && matrix[r][c - 1] == 1) {
+            to = convertToNode(r, c - 1, rows, columns);
+            if (debugMode) {
+              System.out.println("to: " + to);
+            }
+            graph.addEdge(from, to);
+          }
+          if (c < columns - 1 && matrix[r][c + 1] == 1) {
+            to = convertToNode(r, c + 1, rows, columns);
+            if (debugMode) {
+              System.out.println("to: " + to);
+            }
+            graph.addEdge(from, to);
+          }
+        } else {
+          bfs.setOcean(from);
+        }
+      }
+    }
+    for (int f = 0; f < fireballs; f++) {
+      int row = in.nextInt();
+      int column = in.nextInt();
+      int node = convertToNode(row - 1, column - 1, rows, columns);
+      if (debugMode) {
+        System.out.println("row: " + row + " column:" + column + " node: " + node);
+      }
+      results[f] = bfs.countCities(node);
+      if (debugMode) {
+        System.out.println("f: " + f + " results: " + results[f]);
+      }
+    }
+    return results;
+  }
+
+  private static int convertToNode(int row, int column, int rows, int columns) {
+    return row * columns + column;
+  }
+
+  private static class Graph {
+    int verticies;
+    int edges;
+    ArrayList<Integer>[] adj;
+
+    public Graph(int verticies) {
+      this.verticies = verticies;
+      edges = 0;
+      adj = (ArrayList<Integer>[]) new ArrayList[verticies];
+      for (int v = 0; v < verticies; v++) {
+        adj[v] = new ArrayList<Integer>();
+      }
+    }
+
+    public void addEdge(int from, int to) {
+      edges++;
+      adj[from].add(to);
+      adj[to].add(from);
+    }
+
+    public int verticies() {
+      return verticies;
+    }
+
+    public Iterable<Integer> adj(int node) {
+      return adj[node];
+    }
+  }
+
+  private static class BreadthFirstSearch {
+    boolean[] marked;
+    Graph graph;
+
+    BreadthFirstSearch(Graph graph) {
+      this.graph = graph;
+      marked = new boolean[graph.verticies()];
+    }
+
+    private void bfs(int source) {
+      Queue<Integer> queue = new LinkedList<Integer>();
+      queue.add(source);
+      marked[source] = true;
+      if (debugMode) {
+        System.out.println("source: " + source);
+      }
+      while (!queue.isEmpty()) {
+        int node = queue.poll();
+        if (debugMode) {
+          System.out.println("node: " + node);
+        }
+        for (int link : graph.adj(node)) {
+          if (debugMode) {
+            System.out.print("link: " + link + " marked: " + marked[link]);
+          }
+          if (!marked[link]) {
+            marked[link] = true;
+            queue.add(link);
+          }
+        }
+        if (debugMode) {
+          System.out.println();
+        }
+      }
+    }
+
+    public int countCities(int source) {
+      bfs(source);
+      int count = 0;
+      for (int i = 0; i < marked.length; i++) {
+        if (debugMode) {
+          System.out.print("i: " + i + " marked: " + marked[i] + " ");
+        }
+        if (!marked[i]) {
+          ++count;
+        }
+      }
+      if (debugMode) {
+        System.out.println("\ncount: " + count);
+      }
+      return count;
+    }
+
+    public void setOcean(int source) {
+      marked[source] = true;
+    }
+  }
+}

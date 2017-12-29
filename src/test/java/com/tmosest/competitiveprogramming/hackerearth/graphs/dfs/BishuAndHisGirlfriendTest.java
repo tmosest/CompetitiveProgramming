58f8376fa640cd72d7321eeaefc7f8061d69d379
Tyler Moses <tmosest@gmail.com>
2017-12-28 21:15:36 -0500

    Bishu and his Girlfriend, solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/hackerearth/graphs/dfs/BishuAndHisGirlfriendTest.java
@@ -0,0 +1,36 @@
+package com.tmosest.competitiveprogramming.hackerearth.graphs.dfs;
+
+import com.tmosest.competitiveprogramming.hackerrank.HackerrankUtil;
+import java.io.UnsupportedEncodingException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("HackerEarth Algorithms Graphs DFS: Bishu and his Girlfriend")
+public class BishuAndHisGirlfriendTest {
+
+  private static void test(String input, int output, boolean debugMode)
+      throws UnsupportedEncodingException {
+    HackerrankUtil.setSystemIn(input);
+    BishuAndHisGirlfriend.debugMode = debugMode;
+    Assertions.assertEquals(output, BishuAndHisGirlfriend.solve());
+  }
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() throws UnsupportedEncodingException {
+    String input = "6\n"
+        + "1 2\n"
+        + "1 3\n"
+        + "1 4\n"
+        + "2 5\n"
+        + "2 6\n"
+        + "4\n"
+        + "5\n"
+        + "6\n"
+        + "3\n"
+        + "4";
+    int output = 3;
+    test(input, output, false);
+  }
+}

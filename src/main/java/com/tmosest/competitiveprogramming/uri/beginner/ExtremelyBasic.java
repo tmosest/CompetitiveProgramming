8fc20b125c723525863e485370ab29841caa218c
tmosest <tmosest@gmail.com>
2017-12-29 21:14:23 -0500

    Extremely Basic, solved

--- src/main/java/com/tmosest/competitiveprogramming/uri/beginner/ExtremelyBasic.java
+++ src/main/java/com/tmosest/competitiveprogramming/uri/beginner/ExtremelyBasic.java
@@ -4,17 +4,28 @@ import java.util.Scanner;
 
 public class ExtremelyBasic {
 
+  public static boolean debugMode = false;
+
+  /**
+   * Function to test.
+   *
+   * @return String result.
+   */
+  public static String solve() {
+    Scanner in = new Scanner(System.in);
+    int num1 = in.nextInt();
+    int num2 = in.nextInt();
+    in.close();
+    String result = "X = " + (num1 + num2);
+    return result;
+  }
+
   /**
    * Main Function.
    *
    * @param args Main String array
    */
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    int num1 = in.nextInt();
-    int num2 = in.nextInt();
-    in.close();
-    String result = "X = " + (num1 + num2);
-    System.out.println(result);
+    System.out.println(solve());
   }
 }

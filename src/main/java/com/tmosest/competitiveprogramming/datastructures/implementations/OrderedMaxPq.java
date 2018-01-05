e5769c33bf8e113d5f0e06b0fdae59d501aa9940
Tyler Moses <tmosest@gmail.com>
2018-01-05 04:04:11 -0500

    Ordered Max Priority Queue, with ArrayList

--- src/main/java/com/tmosest/competitiveprogramming/datastructures/implementations/OrderedMaxPq.java
+++ src/main/java/com/tmosest/competitiveprogramming/datastructures/implementations/OrderedMaxPq.java
@@ -2,48 +2,87 @@ package com.tmosest.competitiveprogramming.datastructures.implementations;
 
 import com.tmosest.competitiveprogramming.datastructures.MaxPq;
 
+import java.util.ArrayList;
+
+/**
+ * Ordered Array version of a Max Priority Queue.
+ */
 public class OrderedMaxPq<T extends Comparable<T>> implements MaxPq<T> {
 
   int size;
-  T[] orderedArray;
+  ArrayList<T> orderedArray;
 
-  public OrderedMaxPq(int capacity) {
+  /**
+   * Creates an empty priority queue.
+   */
+  public OrderedMaxPq() {
     size = 0;
-    orderedArray = (T[]) new Comparable[capacity];
+    orderedArray = new ArrayList<T>();
   }
 
+  /**
+   * Delete the max element from the queue and return it. O(1).
+   *
+   * @return The max element on the queue.
+   */
   public T delMax() {
-    return orderedArray[--size];
+    return orderedArray.get(--size);
   }
 
+  /**
+   * Determine if the queue is empty or not.
+   *
+   * @return Return true if the size is zero.
+   */
   public boolean isEmpty() {
     return size == 0;
   }
 
+  /**
+   * Insert a new element onto the queue. O(size).
+   *
+   * @param key key to insert.
+   */
   public void insert(T key) {
-    int toInsert = 0;
+    int toInsert = size;
     for (int i = 0; i < size; i++) {
-      if (orderedArray[i].compareTo(key) > 0) {
+      if (orderedArray.get(i).compareTo(key) > 0) {
         toInsert = i;
         break;
       }
     }
     if (toInsert == size) {
-      orderedArray[size] = key;
+      if (size >= orderedArray.size()) {
+        orderedArray.add(key);
+      } else {
+        orderedArray.set(size, key);
+      }
     } else {
       // Need to shift up
       for (int i = size; i > toInsert; i--) {
-        orderedArray[i] = orderedArray[i - 1];
+        if (i >= orderedArray.size()) {
+          orderedArray.add(orderedArray.get(i - 1));
+        } else {
+          orderedArray.set(i, orderedArray.get(i - 1));
+        }
       }
-      orderedArray[toInsert] = key;
+      orderedArray.set(toInsert, key);
     }
     size++;
   }
 
+  /**
+   * Get the max element. O(1).
+   */
   public T max() {
-    return orderedArray[size - 1];
+    return orderedArray.get(size - 1);
   }
 
+  /**
+   * Return the size.
+   *
+   * @return Number of elements in the queue.
+   */
   public int size() {
     return size;
   }

01339615aa60e4837ef3a2bf1eb88691d325ebc3
tmosest <tmosest@gmail.com>
2018-01-11 00:52:15 -0500

    Zero Matrix, solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/crackingcode/arrays/ZeroMatrixTest.java
@@ -0,0 +1,37 @@
+package com.tmosest.competitiveprogramming.crackingcode.arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ZeroMatrixTest {
+/*
+  @Test
+  void testCase0() {
+    int[][] matrix = {
+        {1 , 0},
+        {1 , 1}
+    };
+    int[][] result = {
+        {0 , 0},
+        {1 , 0}
+    };
+    Assertions.assertArrayEquals(result, ZeroMatrix.zeroMatrix(matrix));
+  }
+  */
+
+  @Test
+  void testCase1() {
+    int[][] matrix = {
+        {1 , 1},
+        {1 , 1},
+        {0 , 1}
+    };
+    int[][] result = {
+        {0 , 1},
+        {0 , 1},
+        {0 , 0}
+    };
+    Assertions.assertArrayEquals(result, ZeroMatrix.zeroMatrix(matrix));
+  }
+
+}

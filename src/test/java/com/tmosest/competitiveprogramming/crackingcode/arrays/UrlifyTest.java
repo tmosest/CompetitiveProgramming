cdf23592a80df4f73e9e862ce5c980cffd25d33e
tmosest <tmosest@gmail.com>
2018-01-10 22:53:45 -0500

    Urlify, solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/crackingcode/arrays/UrlifyTest.java
@@ -0,0 +1,14 @@
+package com.tmosest.competitiveprogramming.crackingcode.arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UrlifyTest {
+
+  @Test
+  void testUrlify() {
+    String string = "Mr John Smith    ";
+    Assertions.assertEquals("Mr%20John%20Smith", Urlify.urlify(string.toCharArray()));
+  }
+
+}

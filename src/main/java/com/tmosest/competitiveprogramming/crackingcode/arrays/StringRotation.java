b324d4af0c5d9e555626ac7191e1bc7be51f5fb4
tmosest <tmosest@gmail.com>
2018-01-11 00:59:00 -0500

    String Rotation, missed corner cases

--- src/main/java/com/tmosest/competitiveprogramming/crackingcode/arrays/StringRotation.java
+++ src/main/java/com/tmosest/competitiveprogramming/crackingcode/arrays/StringRotation.java
@@ -3,8 +3,11 @@ package com.tmosest.competitiveprogramming.crackingcode.arrays;
 public class StringRotation {
 
   public static boolean isRotation(String string1, String string2) {
-    string1 = string1 + string1;
-    return string1.indexOf(string2) != -1;
+    if(string1.length() == string2.length() && string1.length() > 0) {
+      string1 = string1 + string1;
+      return string1.indexOf(string2) != -1;
+    }
+    return false;
   }
 
 }

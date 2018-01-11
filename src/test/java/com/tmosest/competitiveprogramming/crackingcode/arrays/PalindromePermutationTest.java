993cc71ca74f5d7f54101eb441247499f3368c58
tmosest <tmosest@gmail.com>
2018-01-10 23:19:06 -0500

    Palindrome Permutation, solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/crackingcode/arrays/PalindromePermutationTest.java
@@ -0,0 +1,25 @@
+package com.tmosest.competitiveprogramming.crackingcode.arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PalindromePermutationTest {
+
+  @Test
+  void trueForNonPalindromePermutations() {
+    Assertions.assertEquals(true, PalindromePermutation.isPalindromePermutation("Tact Coa"));
+    Assertions.assertEquals(true, PalindromePermutation.isPalindromePermutation("tactcoapapa"));
+  }
+
+  @Test
+  void falseForNonPalindromePermutations() {
+    Assertions.assertEquals(false, PalindromePermutation.isPalindromePermutation("Tac Coa"));
+  }
+
+  @Test
+  void noSpecialCharacters() {
+    Assertions.assertEquals(false, PalindromePermutation.isPalindromePermutation("Tac?Coa"));
+    Assertions.assertEquals(true, PalindromePermutation.isPalindromePermutation("Taco?Coat"));
+  }
+
+}

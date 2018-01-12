efec091e3175fb347dad8735399055676c92cbba
tmosest <tmosest@gmail.com>
2018-01-12 02:32:46 -0500

    Sum Links, solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/crackingcode/linkedlists/SumLinksTest.java
@@ -0,0 +1,23 @@
+package com.tmosest.competitiveprogramming.crackingcode.linkedlists;
+
+import com.tmosest.competitiveprogramming.datastructures.implementations.LinkedList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SumLinksTest {
+
+  @Test
+  void shouldSumLinkedList() {
+    LinkedList<Integer> linkedList = new LinkedList<Integer>();
+    linkedList.append(5);
+    linkedList.append(3);
+    linkedList.append(5);
+    LinkedList<Integer> otherLinkedList = new LinkedList<Integer>();
+    otherLinkedList.append(1);
+    otherLinkedList.append(2);
+    otherLinkedList.append(3);
+    Assertions.assertEquals((535 + 123), SumLinks.sumLinks(linkedList.head(), otherLinkedList.head(), false));
+    Assertions.assertEquals((535 + 321), SumLinks.sumLinks(linkedList.head(), otherLinkedList.head(), true));
+  }
+
+}

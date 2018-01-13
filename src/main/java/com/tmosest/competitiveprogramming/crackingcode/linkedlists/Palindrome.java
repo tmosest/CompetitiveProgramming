29607c5c7016ea0144b80921e7d83522807d962d
Tyler Moses <tmosest@gmail.com>
2018-01-12 22:36:21 -0500

    Palindrom, better solution

--- src/main/java/com/tmosest/competitiveprogramming/crackingcode/linkedlists/Palindrome.java
+++ src/main/java/com/tmosest/competitiveprogramming/crackingcode/linkedlists/Palindrome.java
@@ -7,31 +7,27 @@ import java.util.Stack;
 public class Palindrome {
 
   private static Node<Integer> reversedLinkedList(Node<Integer> head) {
-    Stack<Integer> stack = new Stack<Integer>();
+    Node<Integer> reversed = null;
     while (head != null) {
-      stack.push(head.data);
+      // Clone
+      Node<Integer> node = new Node<Integer>();
+      node.data = head.data;
+      node.next = reversed;
+      reversed = node;
       head = head.next;
     }
-    if (stack.empty()) {
-      return null;
-    }
-    Node<Integer> reversed = new Node<Integer>();
-    reversed.data = stack.pop();
-    Node<Integer> pointer = null;
-    Node<Integer> next;
-    while (!stack.empty()) {
-      if (pointer == null) {
-        pointer = new Node<Integer>();
-        pointer.data = stack.pop();
-        reversed.next = pointer;
-      } else {
-        next = new Node<Integer>();
-        next.data = stack.pop();
-        pointer.next = next;
-        pointer = pointer.next;
-      }
-    }
     return reversed;
+  }
+
+  private static boolean isEqual(Node<Integer> firstHead, Node<Integer> secondHead) {
+    while (firstHead != null && secondHead != null) {
+      if (firstHead.data != secondHead.data) {
+        return false;
+      }
+      firstHead = firstHead.next;
+      secondHead = secondHead.next;
+    }
+    return true;
   }
 
   /**
@@ -42,14 +38,7 @@ public class Palindrome {
    */
   public static boolean isPalindrome(Node<Integer> head) {
     Node<Integer> reversed = reversedLinkedList(head);
-    while (head != null) {
-      if (head.data != reversed.data) {
-        return false;
-      }
-      head = head.next;
-      reversed = reversed.next;
-    }
-    return true;
+    return isEqual(head, reversed);
   }
 
 }

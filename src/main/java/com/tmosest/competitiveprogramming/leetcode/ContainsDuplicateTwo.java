0469b8b35629623b7de85c3a238d2f2a241a720f
tmosest <tmosest@gmail.com>
2019-01-02 00:46:23 -0500

    ContainsDuplicateII: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/ContainsDuplicateTwo.java
@@ -0,0 +1,25 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateTwo {
+
+  /**
+   * Determines if there are two duplicates within a given range.
+   * @param nums An array of integers.
+   * @param distance The max allowed distance between the duplicates.
+   * @return True if a dupe was found with the given range difference.
+   */
+  public boolean containsNearbyDuplicate(int[] nums, int distance) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      Integer index = map.get(nums[i]);
+      if (index != null && i - index <= distance) {
+        return true;
+      }
+      map.put(nums[i], i);
+    }
+    return false;
+  }
+}

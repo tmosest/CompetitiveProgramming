2d3efe9973c2954992313ad9ae08f3bf40149f4f
tmosest <tmosest@gmail.com>
2019-01-04 18:49:17 -0500

    BinaryTreePaths: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/BinaryTreePaths.java
@@ -0,0 +1,34 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePaths {
+
+  /**
+   * Function to get all the paths from a root to leaf as strings.
+   * @param root The root node of the binary tree.
+   * @return A List of all the paths as strings.
+   */
+  public List<String> binaryTreePaths(TreeNode root) {
+    List<String> result = new ArrayList<>();
+    helper(root, "", result);
+    return result;
+  }
+
+  private void helper(TreeNode node, String path, List<String> list) {
+    if (node == null) {
+      return;
+    }
+    if (!"".equals(path)) {
+      path += "->";
+    }
+    path += node.val;
+    if (node.left == null && node.right == null) {
+      list.add(path);
+      return;
+    }
+    helper(node.left, path, list);
+    helper(node.right, path, list);
+  }
+}
---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/BinaryTreePathsTest.java
@@ -0,0 +1,32 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("binaryTree")
+@DisplayName("LeetCode: 257. Binary Tree Paths")
+public class BinaryTreePathsTest {
+
+  BinaryTreePaths binaryTreePaths = new BinaryTreePaths();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
+    root.left.right = new TreeNode(5);
+
+    List<String> expected = new LinkedList<>();
+    expected.add("1->2->5");
+    expected.add("1->3");
+    Assertions.assertEquals(expected, binaryTreePaths.binaryTreePaths(root));
+  }
+}

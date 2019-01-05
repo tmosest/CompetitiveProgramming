aa2033f090c5e07381e455cc29b424870af56826
tmosest <tmosest@gmail.com>
2019-01-04 19:02:25 -0500

    IsUgly: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/IsUgly.java
@@ -0,0 +1,25 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class IsUgly {
+
+  /**
+   * Function to determine if a number is ugly or not. Aka if all its
+   * @param num The input number.
+   * @return True if it is ugly.
+   */
+  public boolean isUgly(int num) {
+    if (num == 0) {
+      return false;
+    }
+    while (num % 2 == 0) {
+      num /= 2;
+    }
+    while (num % 3 == 0) {
+      num /= 3;
+    }
+    while (num % 5 == 0) {
+      num /= 5;
+    }
+    return num == 1;
+  }
+}
---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/IsUglyTest.java
@@ -0,0 +1,43 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("number")
+@DisplayName("LeetCode: 263. Ugly Number")
+public class IsUglyTest {
+
+  IsUgly isUgly = new IsUgly();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int num = 6;
+    Assertions.assertTrue(isUgly.isUgly(num));
+  }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    int num = 8;
+    Assertions.assertTrue(isUgly.isUgly(num));
+  }
+
+  @Test
+  @DisplayName("Test Case 2")
+  void testCase2() {
+    int num = 14;
+    Assertions.assertFalse(isUgly.isUgly(num));
+  }
+
+  @Test
+  @DisplayName("Test Case 3")
+  void testCase3() {
+    int num = 5;
+    Assertions.assertTrue(isUgly.isUgly(num));
+  }
+}

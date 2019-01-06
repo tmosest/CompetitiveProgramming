ab8a6f5e592c6f18c7f1689723903cd48563a2a8
tmosest <tmosest@gmail.com>
2019-01-06 09:12:59 -0500

    PaintHouse: update docs

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/PaintHouse.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/PaintHouse.java
@@ -1,6 +1,12 @@
 package com.tmosest.competitiveprogramming.leetcode;
 
 public class PaintHouse {
+
+  /**
+   * Function to compute the min cost of painting a set of houses.
+   * @param costs A matrix of costs per house per color.
+   * @return The min cost to paint all the houses.
+   */
   public int minCost(int[][] costs) {
     if (costs == null || costs.length == 0 || costs[0].length == 0) {
       return 0;

b69cdf9c89e313eb1815fe402789db28641f7aad
tmosest <tmosest@gmail.com>
2019-01-06 09:10:21 -0500

    PaintHouse: works but too slow

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/PaintHouseTest.java
@@ -0,0 +1,34 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("array")
+@Tag("dynamic")
+@DisplayName("LeetCode: 256. Paint House")
+public class PaintHouseTest {
+
+  PaintHouse paintHouse = new PaintHouse();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[][] costs = {
+        {17,2,17},
+        {16,16,5},
+        {14,3,19}
+    };
+    Assertions.assertEquals(10, paintHouse.minCost(costs));
+  }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    int[][] costs = {};
+    Assertions.assertEquals(0, paintHouse.minCost(costs));
+  }
+}

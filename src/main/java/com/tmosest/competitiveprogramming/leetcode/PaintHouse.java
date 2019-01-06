347ab3f918a6ee8dca3c5a7820f2940c575c921d
tmosest <tmosest@gmail.com>
2019-01-06 09:10:21 -0500

    PaintHouse: works but too slow

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/PaintHouse.java
@@ -0,0 +1,30 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class PaintHouse {
+  public int minCost(int[][] costs) {
+    if (costs == null || costs.length == 0 || costs[0].length == 0) {
+      return 0;
+    }
+
+    int min = Integer.MAX_VALUE;
+
+    for (int i = 0; i < costs[0].length; i++) {
+      min = Math.min(min, minCost(costs, 1, i, costs[0][i]));
+    }
+
+    return min;
+  }
+
+  private int minCost(int[][] costs, int index, int previousColor, int cost) {
+    if (index >= costs.length) {
+      return cost;
+    }
+    int min = Integer.MAX_VALUE;
+    for (int i = 0; i < costs[index].length; i++) {
+      if (i != previousColor) {
+        min = Math.min(min, minCost(costs, index + 1, i, cost + costs[index][i]));
+      }
+    }
+    return min;
+  }
+}

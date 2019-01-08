45cb4fe65b304039a05c112678b35b81a1b61e76
tmosest <tmosest@gmail.com>
2019-01-08 16:20:51 -0500

    AddStrings: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/AddStrings.java
@@ -0,0 +1,70 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class AddStrings {
+
+  /**
+   * Function to sum two numbers strings that are numbers.
+   * @param num1 The first number string.
+   * @param num2 The second number string.
+   * @return The sum of the two numbers as strings.
+   */
+  public String addStrings(String num1, String num2) {
+    if (num1 == null && num2 == null) {
+      return null;
+    }
+    if (num1 == null || num2 == null) {
+      return (num1 == null) ? num2 : num1;
+    }
+    boolean hasCarryOver = false;
+    StringBuilder sb = new StringBuilder();
+
+    char[] num1Array = num1.toCharArray();
+    char[] num2Array = num2.toCharArray();
+
+    int minLength = Math.min(num1Array.length, num2Array.length);
+
+    for (int i = 0; i < minLength; i++) {
+      int sum = (num1Array[num1Array.length - 1 - i] - '0')
+          + (num2Array[num2Array.length - 1 - i] - '0');
+      if (hasCarryOver) {
+        sum++;
+        hasCarryOver = false;
+      }
+      if (sum >= 10) {
+        sum -= 10;
+        hasCarryOver = true;
+      }
+      char digit = (char)(sum + '0');
+      sb.append(digit);
+    }
+
+    char[] remainder = null;
+    if (num1Array.length > minLength) {
+      remainder = num1Array;
+    }
+    if (num2Array.length > minLength) {
+      remainder = num2Array;
+    }
+    if (remainder != null) {
+      for (int i = remainder.length - minLength - 1; i >= 0; i--) {
+        int sum = remainder[i] - '0';
+        if (hasCarryOver) {
+          sum++;
+          hasCarryOver = false;
+        }
+        if (sum >= 10) {
+          sum -= 10;
+          hasCarryOver = true;
+        }
+        char digit = (char)(sum + '0');
+        sb.append(digit);
+      }
+    }
+
+    if (hasCarryOver) {
+      sb.append("1");
+    }
+
+    return sb.reverse().toString();
+  }
+}
---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/AddStringsTest.java
@@ -0,0 +1,33 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("string")
+@DisplayName("LeetCode: 415. Add Strings")
+public class AddStringsTest {
+
+  AddStrings addStrings = new AddStrings();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    Assertions.assertEquals("123", addStrings.addStrings("120", "3"));
+  }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    Assertions.assertEquals("100", addStrings.addStrings("99", "1"));
+  }
+
+  @Test
+  @DisplayName("Test Case 2")
+  void testCase2() {
+    Assertions.assertEquals("2", addStrings.addStrings("1", "1"));
+  }
+}

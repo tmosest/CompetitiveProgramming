f7dd5f58e24b4d2a4257b7cb548a1c40e507e63d
tmosest <tmosest@gmail.com>
2019-01-07 21:05:06 -0500

    ValidPerfectSquare: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/ValidWordAbbreviation.java
@@ -0,0 +1,77 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class ValidWordAbbreviation {
+
+  private boolean debugMode = false;
+
+  /**
+   * Function to determine if a string if a valid abbreviation or not.
+   *
+   * @param word The word.
+   * @param abbr The abbreviation.
+   * @return True if it is a valid abbreviation.
+   */
+  public boolean validWordAbbreviation(String word, String abbr) {
+    if (word == null && abbr == null) {
+      return true;
+    }
+
+    if (word == null || abbr == null || (abbr.isEmpty() && !word.isEmpty())) {
+      return false;
+    }
+
+    char[] wordArray = word.toCharArray();
+    char[] abbrArray = abbr.toCharArray();
+    int wordIndex = 0;
+    int abbrIndex = 0;
+    int loopTimes;
+
+    while (abbrIndex < abbrArray.length) {
+      char abbrLetter = abbrArray[abbrIndex];
+      if (debugMode) {
+        System.out.println("abbrLetter: " + abbrLetter);
+      }
+      if (Character.isDigit(abbrLetter)) {
+        loopTimes = 0;
+        if (loopTimes == 0 && abbrLetter == '0') {
+          return false;
+        }
+        loopTimes += (abbrLetter - '0');
+        if (abbrIndex + 1 < abbrArray.length) {
+          abbrLetter = abbrArray[++abbrIndex];
+          while (Character.isDigit(abbrLetter)) {
+            loopTimes *= 10;
+            loopTimes += (abbrLetter - '0');
+            if (abbrIndex + 1 < abbrArray.length) {
+              abbrLetter = abbrArray[++abbrIndex];
+            } else {
+              abbrLetter = 'a';
+            }
+            if (debugMode) {
+              System.out.println("loopTimes: " + loopTimes);
+            }
+          }
+        } else {
+          abbrIndex++;
+        }
+        wordIndex += loopTimes;
+      } else {
+        if (debugMode) {
+          System.out.println("wordIndex: " + wordIndex);
+          System.out.println("wordArray[wordIndex]: " + wordArray[wordIndex]);
+        }
+        if (wordIndex >= wordArray.length || abbrLetter != wordArray[wordIndex]) {
+          return false;
+        }
+        ++wordIndex;
+        ++abbrIndex;
+      }
+    }
+
+    if (wordIndex != wordArray.length) {
+      return false;
+    }
+
+    return true;
+  }
+}
---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/ValidWordAbbreviationTest.java
@@ -0,0 +1,39 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("number")
+@DisplayName("LeetCode: 408. Valid Word Abbreviation")
+public class ValidWordAbbreviationTest {
+
+  ValidWordAbbreviation validWordAbbreviation = new ValidWordAbbreviation();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    Assertions.assertTrue(validWordAbbreviation.validWordAbbreviation("internationalization", "i12iz4n"));
+  }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    Assertions.assertFalse(validWordAbbreviation.validWordAbbreviation("apple", "a2e"));
+  }
+
+  @Test
+  @DisplayName("Test Case 2")
+  void testCase2() {
+    Assertions.assertTrue(validWordAbbreviation.validWordAbbreviation("internationalization", "i5a11o1"));
+  }
+
+  @Test
+  @DisplayName("Test Case 3")
+  void testCase3() {
+    Assertions.assertFalse(validWordAbbreviation.validWordAbbreviation("h1", "11"));
+  }
+}

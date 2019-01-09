7b876b7a945b6d70aeee8377a57cd58f59cb4625
tmosest <tmosest@gmail.com>
2019-01-08 19:40:34 -0500

    WordPattern: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/WordPatternTest.java
@@ -0,0 +1,33 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("string")
+@DisplayName("LeetCode: 290. Word Pattern")
+public class WordPatternTest {
+
+  WordPattern wordPattern = new WordPattern();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    Assertions.assertTrue(wordPattern.wordPattern("abba", "dog cat cat dog"));
+  }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    Assertions.assertFalse(wordPattern.wordPattern("abba", "dog cat cat fish"));
+  }
+
+  @Test
+  @DisplayName("Test Case 2")
+  void testCase2() {
+    Assertions.assertFalse(wordPattern.wordPattern("abba", "dog dog dog dog"));
+  }
+}

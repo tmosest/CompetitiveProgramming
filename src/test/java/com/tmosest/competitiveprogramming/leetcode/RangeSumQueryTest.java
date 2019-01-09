46ab3ea6f68de805f76d7b3b6bc50eeee54064e2
Tyler Moses <tmosest@gmail.com>
2019-01-09 14:11:10 -0500

    Range Sum Query: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/RangeSumQueryTest.java
@@ -0,0 +1,21 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("array")
+@DisplayName("LeetCode: 303. Range Sum Query - Immutable")
+public class RangeSumQueryTest {
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[] array = {0,1,2,3,4,5,6,7};
+    NumArray numArray = new NumArray(array);
+    Assertions.assertEquals(1, numArray.sumRange(0, 1));
+  }
+}

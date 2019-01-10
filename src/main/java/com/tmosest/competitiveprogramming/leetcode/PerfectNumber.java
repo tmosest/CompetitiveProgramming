7e569f77d67561b1c9488a60abb50145796583b7
Tyler Moses <tmosest@gmail.com>
2019-01-10 18:18:32 -0500

    PerfectNumber: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/PerfectNumber.java
@@ -0,0 +1,25 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class PerfectNumber {
+
+  /**
+   * Determines if a number is the sum of its divisors less than itself.
+   * @param num The number to check if perfect.
+   * @return True if the number is perfect and false otherwise.
+   */
+  public boolean checkPerfectNumber(int num) {
+    if (num < 2) {
+      return false;
+    }
+
+    int sum = 1;
+
+    for (int i = 2; i < num; i++) {
+      if (num % i == 0) {
+        sum += i;
+      }
+    }
+
+    return sum == num;
+  }
+}

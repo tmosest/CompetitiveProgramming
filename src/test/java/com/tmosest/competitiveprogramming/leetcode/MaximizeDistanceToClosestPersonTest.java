21ac3cf89cb2d7427df3f6d849e2e4df084a553a
tmosest <tmosest@gmail.com>
2019-01-12 17:35:08 -0500

    MaximizeDistanceToClosestPerson: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/MaximizeDistanceToClosestPersonTest.java
@@ -0,0 +1,33 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("array")
+@DisplayName("LeetCode: 849. Maximize Distance to Closest Person")
+public class MaximizeDistanceToClosestPersonTest {
+
+  MaximizeDistanceToClosestPerson maximizeDistanceToClosestPerson = new MaximizeDistanceToClosestPerson();
+
+  void test(int[] input, int expected) {
+    Assertions.assertEquals(expected, maximizeDistanceToClosestPerson.maxDistToClosest(input));
+  }
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[] input = {1,0,0,0,1,0,1};
+    test(input, 2);
+  }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    int[] input = {1,0,0,0};
+    test(input, 3);
+  }
+}

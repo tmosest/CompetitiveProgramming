e9b457e44e91237ebaad2ea34efdb5ee87c82ac8
tmosest <tmosest@gmail.com>
2019-01-13 16:20:00 -0500

    DiameterOfBinaryTree: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/DiameterOfBinaryTree.java
@@ -0,0 +1,26 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class DiameterOfBinaryTree {
+  private int answer;
+
+  /**
+   * Determine the diameter of a binary tree. (Length of longest path).
+   * @param root The root node.
+   * @return The length of the longest path in the tree.
+   */
+  public int diameterOfBinaryTree(TreeNode root) {
+    answer = 1;
+    depth(root);
+    return answer - 1;
+  }
+
+  private int depth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int left = depth(root.left);
+    int right = depth(root.right);
+    answer = Math.max(answer, left + right + 1);
+    return Math.max(left, right) + 1;
+  }
+}

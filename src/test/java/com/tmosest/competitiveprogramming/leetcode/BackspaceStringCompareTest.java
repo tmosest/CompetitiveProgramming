6df4e4b5488f9edf17f05b516d210304584f88d5
tmosest <tmosest@gmail.com>
2019-01-16 20:55:08 -0500

    BackspaceStringCompare: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/BackspaceStringCompareTest.java
@@ -0,0 +1,39 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("string")
+@DisplayName("LeetCode: 844. Backspace String Compare")
+public class BackspaceStringCompareTest {
+
+  BackspaceStringCompare backspaceStringCompare = new BackspaceStringCompare();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    Assertions.assertTrue(backspaceStringCompare.backspaceCompare("ab#c", "ad#c"));
+  }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    Assertions.assertTrue(backspaceStringCompare.backspaceCompare("ab##", "c#d#"));
+  }
+
+  @Test
+  @DisplayName("Test Case 2")
+  void testCase2() {
+    Assertions.assertTrue(backspaceStringCompare.backspaceCompare("a##c", "#a#c"));
+  }
+
+  @Test
+  @DisplayName("Test Case 3")
+  void testCase3() {
+    Assertions.assertFalse(backspaceStringCompare.backspaceCompare("a#c", "b"));
+  }
+}

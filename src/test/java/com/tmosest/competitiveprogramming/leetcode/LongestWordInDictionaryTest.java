94bdfaa9b7799102dc90b2547f88566b9ff47e9f
tmosest <tmosest@gmail.com>
2019-01-23 21:50:00 -0500

    LongestWordInDictionary: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/LongestWordInDictionaryTest.java
@@ -0,0 +1,24 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("array")
+@Tag("string")
+@Tag("trie")
+@DisplayName("LeetCode: 720. Longest Word in Dictionary")
+public class LongestWordInDictionaryTest {
+
+  LongestWordInDictionary longestWordInDictionary = new LongestWordInDictionary();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    String[] words = {"w","wo","wor","worl", "world"};
+    Assertions.assertEquals("world", longestWordInDictionary.longestWord(words));
+  }
+}

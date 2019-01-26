211156b84a69b69efa14b82a9ac9fa2ee2a40a19
tmosest <tmosest@gmail.com>
2019-01-25 20:38:33 -0500

    SumOfTwoIntegers: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/SumOfTwoIntegersTest.java
@@ -0,0 +1,21 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("string")
+@DisplayName("LeetCode: 371. Sum of Two Integers")
+public class SumOfTwoIntegersTest {
+
+  SumOfTwoIntegers sumOfTwoIntegers = new SumOfTwoIntegers();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    Assertions.assertEquals(3, sumOfTwoIntegers.getSum(1, 2));
+  }
+}

811c43ef2fcbad90133dd0b6caea054cd0bcf838
tmosest <tmosest@gmail.com>
2019-01-29 21:47:05 -0500

    SentenceSimilarity: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/SentenceSimilarityTest.java
@@ -0,0 +1,29 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("array")
+@Tag("string")
+@DisplayName("LeetCode: 734. Sentence Similarity")
+public class SentenceSimilarityTest {
+
+  SentenceSimilarity sentenceSimilarity = new SentenceSimilarity();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    String[] words1 = {"great", "acting", "skills"};
+    String[] words2 = {"fine", "drama", "talent"};
+    String[][] pairs = {
+        {"great", "fine"},
+        {"acting", "drama"},
+        {"skills", "talent"}
+    };
+    Assertions.assertTrue(sentenceSimilarity.areSentencesSimilar(words1, words2, pairs));
+  }
+}

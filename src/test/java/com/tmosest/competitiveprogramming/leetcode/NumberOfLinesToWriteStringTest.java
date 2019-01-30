cef12c1e2159c344ec24c86f9cda0a89515ca2a8
tmosest <tmosest@gmail.com>
2019-01-29 21:34:53 -0500

    NumberOfLinesToWriteString: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/NumberOfLinesToWriteStringTest.java
@@ -0,0 +1,24 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("array")
+@Tag("string")
+@DisplayName("LeetCode: 717. 1-bit and 2-bit Characters")
+public class NumberOfLinesToWriteStringTest {
+
+  NumberOfLinesToWriteString numberOfLinesToWriteString = new NumberOfLinesToWriteString();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[] widths = {10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10};
+    int[] expected = {3, 60};
+    Assertions.assertArrayEquals(expected, numberOfLinesToWriteString.numberOfLines(widths, "abcdefghijklmnopqrstuvwxyz"));
+  }
+}

e6527069b9e8d8efad82696a7526ef56b3158794
tmosest <tmosest@gmail.com>
2019-02-04 19:46:33 -0500

    MagicSquaresInGrid: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/MagicSquaresInGridTest.java
@@ -0,0 +1,26 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("matrix")
+@DisplayName("LeetCode: 840. Magic Squares In Grid")
+public class MagicSquaresInGridTest {
+
+  MagicSquaresInGrid magicSquaresInGrid = new MagicSquaresInGrid();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[][] input = {
+        {4,3,8,4},
+        {9,5,1,9},
+        {2,7,6,2}
+    };
+    Assertions.assertEquals(1, magicSquaresInGrid.numMagicSquaresInside(input));
+  }
+}

0c08961b205fa08ff008e8cfe30a7dcc2df67653
tmosest <tmosest@gmail.com>
2019-02-05 21:11:18 -0500

    SumOfEvenNumbersAfterQueries: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/SumOfEvenNumbersAfterQueries.java
@@ -0,0 +1,34 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class SumOfEvenNumbersAfterQueries {
+
+  /**
+   * Find sum of even numbers after an array queries.
+   * @param input The input array.
+   * @param queries An array of queries.
+   * @return An array of results after queries.
+   */
+  public int[] sumEvenAfterQueries(int[] input, int[][] queries) {
+    int[] results = new int[queries.length];
+    int sum = 0;
+    for (int i : input) {
+      if (i % 2 == 0) {
+        sum += i;
+      }
+    }
+    for (int i = 0; i < queries.length; i++) {
+      int[] query = queries[i];
+      int value = query[0];
+      int index = query[1];
+      if (input[index] % 2 == 0) {
+        sum -= input[index];
+      }
+      input[index] += value;
+      if (input[index] % 2 == 0) {
+        sum += input[index];
+      }
+      results[i] = sum;
+    }
+    return results;
+  }
+}

d7e187a6866ad7b85f6567c0449754121cfdb68d
tmosest <tmosest@gmail.com>
2019-02-05 21:11:18 -0500

    SumOfEvenNumbersAfterQueries: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/SumOfEvenNumbersAfterQueriesTest.java
@@ -0,0 +1,26 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("array")
+@DisplayName("LeetCode: 985. Sum of Even Numbers After Queries")
+public class SumOfEvenNumbersAfterQueriesTest {
+
+  SumOfEvenNumbersAfterQueries sumOfEvenNumbersAfterQueries = new SumOfEvenNumbersAfterQueries();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[] input = {1,2,3,4};
+    int[][] queiries = {
+        {1,0},{-3,1},{-4,0},{2,3}
+    };
+    int[] expected = {8,6,2,4};
+    Assertions.assertArrayEquals(expected, sumOfEvenNumbersAfterQueries.sumEvenAfterQueries(input, queiries));
+  }
+}

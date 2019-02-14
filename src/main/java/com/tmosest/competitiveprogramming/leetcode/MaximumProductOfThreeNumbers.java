aba5aaadd4c5dec767b95cd60f5a9f30ac8de41a
tmosest <tmosest@gmail.com>
2019-02-14 14:51:45 -0500

    MaximumProductOfThreeNumbers: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,23 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import java.util.Arrays;
+
+public class MaximumProductOfThreeNumbers {
+
+  /**
+   * Determine the product of the three largest numbers.
+   * @param nums An array of integers.
+   * @return The largest product.
+   */
+  public int maximumProduct(int[] nums) {
+    Arrays.sort(nums);
+    int length = nums.length;
+    if (length < 3) {
+      return 0;
+    }
+    return Math.max(
+        nums[0] * nums[1] * nums[nums.length - 1],
+        nums[length - 1] * nums[length - 2] * nums[length - 3]
+    );
+  }
+}

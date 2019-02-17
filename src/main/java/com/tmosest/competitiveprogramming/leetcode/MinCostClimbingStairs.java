201b297b43ca4e212ea1f80ec9167db1f44a5c25
tmosest <tmosest@gmail.com>
2019-02-16 20:31:40 -0500

    MinCostClimbingStairs: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/MinCostClimbingStairs.java
@@ -0,0 +1,20 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class MinCostClimbingStairs {
+
+  /**
+   * Determine the min cost to reach the top given you can pick between two steps.
+   * @param cost Array of integers which are the costs.
+   * @return The min cost to reach the top.
+   */
+  public int minCostClimbingStairs(int[] cost) {
+    int stepOne = 0;
+    int stepTwo = 0;
+    for (int i = cost.length - 1; i >= 0; --i) {
+      int f0 = cost[i] + Math.min(stepOne, stepTwo);
+      stepTwo = stepOne;
+      stepOne = f0;
+    }
+    return Math.min(stepOne, stepTwo);
+  }
+}

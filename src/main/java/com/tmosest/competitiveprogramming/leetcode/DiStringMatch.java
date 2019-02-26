ffd27e8e49ddb02631c2accddf04877b2e5b871e
tmosest <tmosest@gmail.com>
2019-02-26 18:12:51 -0500

    DiStringMatch: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/DiStringMatch.java
@@ -0,0 +1,25 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class DiStringMatch {
+
+  /**
+   * DI string match.
+   * @param str The string to search.
+   * @return An array of indexes.
+   */
+  public int[] diStringMatch(String str) {
+    int size = str.length();
+    int lo = 0;
+    int hi = size;
+    int[] ans = new int[size + 1];
+    for (int i = 0; i < size; ++i) {
+      if (str.charAt(i) == 'I') {
+        ans[i] = lo++;
+      } else {
+        ans[i] = hi--;
+      }
+    }
+    ans[size] = lo;
+    return ans;
+  }
+}

259197693d7435dbd0cd4142c11e97ac9bc34845
tmosest <tmosest@gmail.com>
2019-02-26 20:48:23 -0500

    MaxConsecutiveOnes: solve

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/MaxConsecutiveOnesTest.java
@@ -0,0 +1,24 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("array")
+@DisplayName("LeetCode: 485. Max Consecutive Ones")
+public class MaxConsecutiveOnesTest {
+
+  MaxConsecutiveOnes maxConsecutiveOnes = new MaxConsecutiveOnes();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[] input = {
+        1,1,0,1,1,1
+    };
+    Assertions.assertEquals(3, maxConsecutiveOnes.findMaxConsecutiveOnes(input));
+  }
+}

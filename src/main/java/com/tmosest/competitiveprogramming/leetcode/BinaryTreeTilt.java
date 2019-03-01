13dc6d6e75f26edbe031dec38ceb826751e49eee
tmosest <tmosest@gmail.com>
2019-03-01 18:58:23 -0500

    BinaryTreeTilt: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/BinaryTreeTilt.java
@@ -0,0 +1,27 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class BinaryTreeTilt {
+
+  private int tilt = 0;
+
+  /**
+   * Find the tilt which is the absolute difference between right and left sum.
+   * @param root The root node.
+   * @return The tilt.
+   */
+  public int findTilt(TreeNode root) {
+    tilt = 0;
+    traverse(root);
+    return tilt;
+  }
+
+  private int traverse(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int left = traverse(root.left);
+    int right = traverse(root.right);
+    tilt += Math.abs(left - right);
+    return left + right + root.val;
+  }
+}

410ba3f6b791f9d46451cefdcff01f7bb9a235ee
tmosest <tmosest@gmail.com>
2019-03-01 19:02:30 -0500

    BaseballGame: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/BaseballGame.java
@@ -0,0 +1,36 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import java.util.Stack;
+
+public class BaseballGame {
+
+  /**
+   * Points for baseball game.
+   * @param ops The scores.
+   * @return The call points.
+   */
+  public int calPoints(String[] ops) {
+    Stack<Integer> stack = new Stack<>();
+
+    for (String op : ops) {
+      if (op.equals("+")) {
+        int top = stack.pop();
+        int newtop = top + stack.peek();
+        stack.push(top);
+        stack.push(newtop);
+      } else if (op.equals("C")) {
+        stack.pop();
+      } else if (op.equals("D")) {
+        stack.push(2 * stack.peek());
+      } else {
+        stack.push(Integer.valueOf(op));
+      }
+    }
+
+    int ans = 0;
+    for (int score : stack) {
+      ans += score;
+    }
+    return ans;
+  }
+}
---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/BaseballGameTest.java
@@ -0,0 +1,24 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("array")
+@DisplayName("LeetCode: 682. Baseball Game")
+public class BaseballGameTest {
+
+  BaseballGame baseballGame = new BaseballGame();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    String[] input = {
+        "5","2","C","D","+"
+    };
+    Assertions.assertEquals(30, baseballGame.calPoints(input));
+  }
+}

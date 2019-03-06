1f1d4321ce2afa90b61adbc5b67c964446fc7040
tmosest <tmosest@gmail.com>
2019-03-06 10:34:40 -0500

    KdiffPairsInAnArray: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/KdiffPairsInAnArrayTest.java
@@ -0,0 +1,24 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("array")
+@DisplayName("LeetCode: 532. K-diff Pairs in an Array")
+public class KdiffPairsInAnArrayTest {
+
+  KdiffPairsInAnArray kdiffPairsInAnArray = new KdiffPairsInAnArray();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[] input = {
+        3, 1, 4, 1, 5
+    };
+    Assertions.assertEquals(2, kdiffPairsInAnArray.findPairs(input, 2));
+  }
+}

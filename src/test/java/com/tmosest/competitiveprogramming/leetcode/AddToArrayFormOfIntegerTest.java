5aeb7747a9bc524dd11f1b4ed1466f4fdf7167eb
tmosest <tmosest@gmail.com>
2019-03-10 15:34:28 -0400

    AddToArrayFormOfInteger: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/AddToArrayFormOfIntegerTest.java
@@ -0,0 +1,51 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("array")
+@DisplayName("LeetCode: 989. Add to Array-Form of Integer")
+public class AddToArrayFormOfIntegerTest {
+
+  AddToArrayFormOfInteger addToArrayFormOfInteger = new AddToArrayFormOfInteger();
+
+  private void test(int[] input, int toAdd, Integer[] output) {
+    List<Integer> expected = Arrays.asList(output);
+    List<Integer> actual = addToArrayFormOfInteger.addToArrayForm(input, toAdd);
+    Assertions.assertEquals(expected.size(), actual.size());
+
+    for (int i = 0; i < expected.size(); i++) {
+      Assertions.assertEquals(expected.get(i), actual.get(i));
+    }
+  }
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[] input = {
+        1,2,0,0
+    };
+    Integer[] output = {
+        1,2,3,4
+    };
+    test(input, 34, output);
+  }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    int[] input = {
+        2,1,5
+    };
+    Integer[] output = {
+        1,0,2,1
+    };
+    test(input, 806, output);
+  }
+}

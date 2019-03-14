834a3a09bb8381e356e7938c3396698074525362
tmosest <tmosest@gmail.com>
2019-03-13 21:19:00 -0400

    RangeAdditionTwo: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/RangeAdditionTwoTest.java
@@ -0,0 +1,22 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("matrix")
+@DisplayName("LeetCode: 598. Range Addition II")
+public class RangeAdditionTwoTest {
+
+  RangeAdditionTwo rangeAdditionTwo = new RangeAdditionTwo();
+  
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[][] input = {{2,2},{3,3}};
+    Assertions.assertEquals(4, rangeAdditionTwo.maxCount(3,3, input));
+  }
+}

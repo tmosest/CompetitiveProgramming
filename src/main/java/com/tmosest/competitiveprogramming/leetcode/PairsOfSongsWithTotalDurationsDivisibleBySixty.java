8f88fc48e1324d46b103712dac19e962aa21d941
tmosest <tmosest@gmail.com>
2019-03-17 15:35:31 -0400

    PairsOfSongsWithTotalDurationsDivisibleBySixty: solved

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/PairsOfSongsWithTotalDurationsDivisibleBySixty.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/PairsOfSongsWithTotalDurationsDivisibleBySixty.java
@@ -1,20 +1,33 @@
 package com.tmosest.competitiveprogramming.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PairsOfSongsWithTotalDurationsDivisibleBySixty {
 
   /**
    * Determine the number of pairs whose sum is divisible by 60.
-   * @param time An array of times.
+   * @param times An array of times.
    * @return The number of paris divisible by sixty.
    */
-  public int numPairsDivisibleBy60(int[] time) {
+  public int numPairsDivisibleBy60(int[] times) {
     int result = 0;
-    for (int i = 0; i < time.length - 1; i++) {
-      for (int j = i + 1; j < time.length; j++) {
-        if ((time[i] + time[j]) % 60 == 0) {
-          ++result;
-        }
+    if (times.length < 2) {
+      return result;
+    }
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < times.length; i++) {
+      times[i] = times[i] % 60;
+      if (map.containsKey(60 - times[i])) {
+        result += map.get(60 - times[i]);
       }
+      Integer count = map.get(times[i]);
+      count = (count == null) ? 1 : count + 1;
+      map.put(times[i], count);
+    }
+    if (map.containsKey(0)) {
+      int count = map.get(0);
+      result += (count - 1) * count / 2;
     }
     return result;
   }

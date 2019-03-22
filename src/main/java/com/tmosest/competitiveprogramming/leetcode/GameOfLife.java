07dfab137912691f21dd90fe95693ee9618401f1
tmosest <tmosest@gmail.com>
2019-03-22 19:41:31 -0400

    GameOfLife: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/GameOfLife.java
@@ -0,0 +1,96 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class GameOfLife {
+
+  private int[][] lifeBoard;
+  private int row = 0;
+  private int col = 0;
+  private static int dieing = -1;
+  private static int becomingAlive = 2;
+  private static int dead = 0;
+  private static int live = 1;
+
+  /**
+   * Game of life simulation.
+   * @param board The starting board.
+   */
+  public void gameOfLife(int[][] board) {
+    lifeBoard = board;
+    for (row = 0; row < lifeBoard.length; row++) {
+      for (col = 0; col < lifeBoard[row].length; col++) {
+        int neighborCount = neighborCount();
+        if (neighborCount < 2) {
+          if (lifeBoard[row][col] == live) {
+            lifeBoard[row][col] = dieing;
+          }
+        } else if (neighborCount >= 2 && neighborCount <= 3) {
+          if (lifeBoard[row][col] == live) {
+            lifeBoard[row][col] = live;
+          } else {
+            if (neighborCount == 3 && lifeBoard[row][col] == dead) {
+              lifeBoard[row][col] = becomingAlive;
+            }
+          }
+        } else {
+          if (lifeBoard[row][col] == live) {
+            lifeBoard[row][col] = dieing;
+          }
+        }
+      }
+    }
+    for (row = 0; row < lifeBoard.length; row++) {
+      for (col = 0; col < lifeBoard[row].length; col++) {
+        if (lifeBoard[row][col] == dieing) {
+          lifeBoard[row][col] = dead;
+        } else if (lifeBoard[row][col] == becomingAlive) {
+          lifeBoard[row][col] = live;
+        }
+      }
+    }
+  }
+
+  private int neighborCount() {
+    int count = 0;
+    if (row - 1 >= 0) {
+      if (lifeBoard[row - 1][col] == dieing || lifeBoard[row - 1][col] == live) {
+        ++count;
+      }
+      if (col - 1 >= 0) {
+        if (lifeBoard[row - 1][col - 1] == dieing || lifeBoard[row - 1][col - 1] == live) {
+          ++count;
+        }
+      }
+      if (col + 1 < lifeBoard[0].length) {
+        if (lifeBoard[row - 1][col + 1] == dieing || lifeBoard[row - 1][col + 1] == live) {
+          ++count;
+        }
+      }
+    }
+    if (row + 1 < lifeBoard.length) {
+      if (lifeBoard[row + 1][col] == dieing || lifeBoard[row + 1][col] == live) {
+        ++count;
+      }
+      if (col - 1 >= 0) {
+        if (lifeBoard[row + 1][col - 1] == dieing || lifeBoard[row + 1][col - 1] == live) {
+          ++count;
+        }
+      }
+      if (col + 1 < lifeBoard[0].length) {
+        if (lifeBoard[row + 1][col + 1] == dieing || lifeBoard[row + 1][col + 1] == live) {
+          ++count;
+        }
+      }
+    }
+    if (col - 1 >= 0) {
+      if (lifeBoard[row][col - 1] == dieing || lifeBoard[row][col - 1] == live) {
+        ++count;
+      }
+    }
+    if (col + 1 < lifeBoard[0].length) {
+      if (lifeBoard[row][col + 1] == dieing || lifeBoard[row][col + 1] == live) {
+        ++count;
+      }
+    }
+    return count;
+  }
+}
---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/GameOfLifeTest.java
@@ -0,0 +1,34 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("binaryTree")
+@Tag("medium")
+@DisplayName("LeetCode: 289. Game of Life")
+public class GameOfLifeTest {
+
+  GameOfLife gameOfLife = new GameOfLife();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[][] input = {
+        {0,1,0},
+        {0,0,1},
+        {1,1,1},
+        {0,0,0}
+    };
+    int[][] expected = {
+        {0,0,0},
+        {1,0,1},
+        {0,1,1},
+        {0,1,0}
+    };
+    gameOfLife.gameOfLife(input);
+    Assertions.assertArrayEquals(expected, input);
+  }
+}

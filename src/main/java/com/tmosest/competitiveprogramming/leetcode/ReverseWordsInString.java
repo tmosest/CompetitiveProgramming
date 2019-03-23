09e1daf5032143408a974a33a72073afe338fc20
tmosest <tmosest@gmail.com>
2019-03-23 14:12:05 -0400

    ReverseWordsInString: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/ReverseWordsInString.java
@@ -0,0 +1,26 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class ReverseWordsInString {
+
+  /**
+   * Reverses the words in a sentence.
+   * @param sentence The sentence to go through.
+   * @return The sentence with the words reversed and no duplicate spaces.
+   */
+  public String reverseWords(String sentence) {
+    StringBuilder stringBuilder = new StringBuilder();
+    String[] words = sentence.trim().split(" ");
+    for (int i = words.length - 1; i >= 0; i--) {
+      String wordTrimmed = words[i].trim();
+      if (wordTrimmed.equals("")) {
+        continue;
+      }
+      stringBuilder.append(words[i].trim());
+      stringBuilder.append(" ");
+    }
+    if (stringBuilder.length() > 0) {
+      stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+    }
+    return stringBuilder.toString();
+  }
+}

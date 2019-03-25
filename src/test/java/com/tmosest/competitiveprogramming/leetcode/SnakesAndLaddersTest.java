5473a64cfaa8870154c85e6b20a08318bd02f93b
tmosest <tmosest@gmail.com>
2019-03-25 18:26:11 -0400

    SnakesAndLadders: solved

--- src/test/java/com/tmosest/competitiveprogramming/leetcode/SnakesAndLaddersTest.java
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/SnakesAndLaddersTest.java
@@ -28,9 +28,8 @@ public class SnakesAndLaddersTest {
     Assertions.assertEquals(4, snakesAndLadders.snakesAndLadders(board));
   }
 
-  /*
   @Test
-  @DisplayName("Test Case 0")
+  @DisplayName("Test Case 1")
   public void testCase1() {
     int[][] board = {
         {-1,1,2,-1},
@@ -40,5 +39,41 @@ public class SnakesAndLaddersTest {
     };
     Assertions.assertEquals(2, snakesAndLadders.snakesAndLadders(board));
   }
-  */
+
+  @Test
+  @DisplayName("Test Case 2")
+  public void testCase2() {
+    int[][] board = {
+        {-1, 4, -1},
+        {6, 2, 6},
+        {-1, 3, -1}
+    };
+    Assertions.assertEquals(2, snakesAndLadders.snakesAndLadders(board));
+  }
+
+  @Test
+  @DisplayName("Test Case 3")
+  public void testCase3() {
+    int[][] board = {
+        {-1,15,9,1,-1},
+        {-1,-1,10,5,19},
+        {18,-1,23,9,-1},
+        {1,13,-1,16,20},
+        {-1,10,10,25,13}
+    };
+    Assertions.assertEquals(1, snakesAndLadders.snakesAndLadders(board));
+  }
+
+  @Test
+  @DisplayName("Test Case 4")
+  public void testCase4() {
+    int[][] board = {
+        {-1,-1,2,21,-1},
+        {16,-1,24,-1,4},
+        {2,3,-1,-1,-1},
+        {-1,11,23,18,-1},
+        {-1,-1,-1,23,-1}
+    };
+    Assertions.assertEquals(2, snakesAndLadders.snakesAndLadders(board));
+  }
 }

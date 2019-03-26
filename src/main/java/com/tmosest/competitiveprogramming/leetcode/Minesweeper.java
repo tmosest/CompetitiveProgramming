fdb7bc6f5c89f6c86c6fdc410c69be3786f2d26a
Tyler Moses <tmosest@gmail.com>
2019-03-26 11:06:12 -0400

    Minesweeper: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/Minesweeper.java
@@ -0,0 +1,82 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class Minesweeper {
+
+  /**
+   * Plays one turn of minesweeper.
+   * @param board The state of the current board.
+   * @param click The location of the click.
+   * @return The new state of the board.
+   */
+  public char[][] updateBoard(char[][] board, int[] click) {
+    char[][] result = board.clone();
+    dfs(result, click[0], click[1]);
+    return result;
+  }
+
+  private static void dfs(char[][] board, int row, int col) {
+    if (row >= board.length || row < 0 || col >= board[0].length || col < 0 || board[row][col] == 'B') {
+      return;
+    }
+    // handle mine
+    if (board[row][col] == 'M') {
+      board[row][col] = 'X';
+      return;
+    }
+    // handle point next to a mine
+    int bombCount = 0;
+    if (row - 1 >= 0) {
+      if (board[row - 1][col] == 'M') {
+        bombCount++;
+      }
+      if (col - 1 >= 0) {
+        if (board[row - 1][col - 1] == 'M') {
+          bombCount++;
+        }
+      }
+      if (col + 1 < board[0].length) {
+        if (board[row - 1][col + 1] == 'M') {
+          bombCount++;
+        }
+      }
+    }
+    if (row + 1 < board.length) {
+      if (board[row + 1][col] == 'M') {
+        bombCount++;
+      }
+      if (col - 1 >= 0) {
+        if (board[row + 1][col - 1] == 'M') {
+          bombCount++;
+        }
+      }
+      if (col + 1 < board[0].length) {
+        if (board[row + 1][col + 1] == 'M') {
+          bombCount++;
+        }
+      }
+    }
+    if (col - 1 >= 0) {
+      if (board[row][col - 1] == 'M') {
+        bombCount++;
+      }
+    }
+    if (col + 1 < board[0].length) {
+      if (board[row][col + 1] == 'M') {
+        bombCount++;
+      }
+    }
+    if (bombCount == 0) {
+      board[row][col] = 'B';
+      dfs(board, row - 1, col);
+      dfs(board, row - 1, col - 1);
+      dfs(board, row - 1, col + 1);
+      dfs(board, row + 1, col);
+      dfs(board, row + 1, col - 1);
+      dfs(board, row + 1, col + 1);
+      dfs(board, row, col - 1);
+      dfs(board, row, col + 1);
+      return;
+    }
+    board[row][col] = (char)(bombCount + '0');
+  }
+}
---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/MinesweeperTest.java
@@ -0,0 +1,35 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("medium")
+@Tag("depthFirstSearch")
+@Tag("matrix")
+@DisplayName("LeetCode: 529. Minesweeper")
+public class MinesweeperTest {
+
+  Minesweeper minesweeper = new Minesweeper();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    char[][] board = {
+        {'E', 'E', 'E', 'E', 'E'},
+        {'E', 'E', 'M', 'E', 'E'},
+        {'E', 'E', 'E', 'E', 'E'},
+        {'E', 'E', 'E', 'E', 'E'}
+    };
+    int[] click = {3, 0};
+    char[][] expected = {
+        {'B', '1', 'E', '1', 'B'},
+        {'B', '1', 'M', '1', 'B'},
+        {'B', '1', '1', '1', 'B'},
+        {'B', 'B', 'B', 'B', 'B'}
+    };
+    Assertions.assertArrayEquals(expected, minesweeper.updateBoard(board, click));
+  }
+}

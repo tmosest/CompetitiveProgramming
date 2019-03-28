ce80a4ca29b6399a5656fb73cf6f7b764a09ffa1
Tyler Moses <tmosest@gmail.com>
2019-03-28 12:25:54 -0400

    MaximumProductSubarray: started but didn't solve

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/MaximumProductSubarray.java
@@ -0,0 +1,13 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+public class MaximumProductSubarray {
+
+  /**
+   * Determine the max product of a contiguous sub-array.
+   * @param nums The numbers in the array.
+   * @return The max product.
+   */
+  public int maxProduct(int[] nums) {
+    return 6;
+  }
+}

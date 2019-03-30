72eafcff9ab686c4be55dcfdb057e9fd0db49711
tmosest <tmosest@gmail.com>
2019-03-30 13:55:14 -0400

    ReorderList: solved

---
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/ReorderList.java
@@ -0,0 +1,32 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReorderList {
+
+  /**
+   * Reorders a list with a zip-zag type order.
+   * @param head The head of the list.
+   */
+  public void reorderList(ListNode head) {
+    List<ListNode> nodes = new ArrayList<>();
+    while (head != null) {
+      nodes.add(head);
+      head = head.next;
+    }
+    if (nodes.size() < 2) {
+      return;
+    }
+    for (int i = 0; i <= nodes.size() / 2; i++) {
+      ListNode node = nodes.get(i);
+      if (i == nodes.size() / 2) {
+        node.next = null;
+      } else {
+        ListNode alt = nodes.get(nodes.size() - 1 - i);
+        node.next = alt;
+        alt.next = nodes.get(i + 1);
+      }
+    }
+  }
+}

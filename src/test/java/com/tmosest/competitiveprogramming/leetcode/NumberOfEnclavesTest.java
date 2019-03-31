59c7577fb305a8eedabb029b68a21ac7c59ccceb
tmosest <tmosest@gmail.com>
2019-03-31 17:25:03 -0400

    NumberOfEnclaves: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/NumberOfEnclavesTest.java
@@ -0,0 +1,43 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("medium")
+@Tag("depthFirstSearch")
+@Tag("matrix")
+@DisplayName("LeetCode: 1020. Number of Enclaves")
+public class NumberOfEnclavesTest {
+
+  NumberOfEnclaves numberOfEnclaves = new NumberOfEnclaves();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    int[][] input = {
+        {0,0,0,0},{1,0,1,0},{0,1,1,0},{0,0,0,0}
+    };
+    Assertions.assertEquals(3, numberOfEnclaves.numEnclaves(input));
+  }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    int[][] input = {
+        {0,1,1,0},{0,0,1,0},{0,0,1,0},{0,0,0,0}
+    };
+    Assertions.assertEquals(0, numberOfEnclaves.numEnclaves(input));
+  }
+
+  @Test
+  @DisplayName("Test Case 2")
+  void testCase2() {
+    int[][] input = {
+        {0,0,0,1,1,1,0,1,0,0},{1,1,0,0,0,1,0,1,1,1},{0,0,0,1,1,1,0,1,0,0},{0,1,1,0,0,0,1,0,1,0},{0,1,1,1,1,1,0,0,1,0},{0,0,1,0,1,1,1,1,0,1},{0,1,1,0,0,0,1,1,1,1},{0,0,1,0,0,1,0,1,0,1},{1,0,1,0,1,1,0,0,0,0},{0,0,0,0,1,1,0,0,0,1}
+    };
+    Assertions.assertEquals(3, numberOfEnclaves.numEnclaves(input));
+  }
+}

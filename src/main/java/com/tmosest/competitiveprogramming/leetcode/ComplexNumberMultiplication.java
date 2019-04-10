e1a98714f2b80f2a5f511746abd2c345bac875f3
tmosest <tmosest@gmail.com>
2019-04-09 20:45:35 -0400

    ComplexNumber: utility class

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/ComplexNumberMultiplication.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/ComplexNumberMultiplication.java
@@ -1,38 +1,8 @@
 package com.tmosest.competitiveprogramming.leetcode;
 
+import com.tmosest.competitiveprogramming.utils.ComplexNumber;
+
 public class ComplexNumberMultiplication {
-
-  private static class ComplexNumber {
-    int one;
-    int two;
-
-    private ComplexNumber(int one, int two) {
-      this.one = one;
-      this.two = two;
-    }
-
-    private ComplexNumber(String string) {
-      String[] vales = string.split("\\+");
-      this.one = Integer.valueOf(vales[0]);
-      this.two = Integer.valueOf(vales[1].replace("i", ""));
-    }
-
-    private ComplexNumber multiply(ComplexNumber other) {
-      int one = this.one * other.one - this.two * other.two;
-      int two = this.two * other.one + other.two * this.one;
-      return new ComplexNumber(one, two);
-    }
-
-    public String toString() {
-      StringBuilder stringBuilder = new StringBuilder();
-      stringBuilder.append(one);
-      stringBuilder.append("+");
-      stringBuilder.append(two);
-      stringBuilder.append("i");
-      return stringBuilder.toString();
-    }
-  }
-
   /**
    * Perform complex multiplication.
    * @param complexOne First complex number.

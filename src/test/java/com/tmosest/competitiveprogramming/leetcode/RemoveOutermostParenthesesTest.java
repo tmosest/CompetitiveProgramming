ddb01893e27de7ed3fa14682a20121c1f2f6aa26
tmosest <tmosest@gmail.com>
2019-04-09 21:38:32 -0400

    RemoveOutermostParentheses: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/RemoveOutermostParenthesesTest.java
@@ -0,0 +1,33 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("easy")
+@Tag("string")
+@DisplayName("LeetCode: 1021. Remove Outermost Parentheses")
+public class RemoveOutermostParenthesesTest {
+
+  RemoveOutermostParentheses removeOutermostParentheses = new RemoveOutermostParentheses();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    Assertions.assertEquals("()()()", removeOutermostParentheses.removeOuterParentheses("(()())(())"));
+  }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    Assertions.assertEquals("", removeOutermostParentheses.removeOuterParentheses("()()"));
+  }
+
+  @Test
+  @DisplayName("Test Case 2")
+  void testCase2() {
+    Assertions.assertEquals("()()()()(())", removeOutermostParentheses.removeOuterParentheses("(()())(())(()(()))"));
+  }
+}

cfc24c4ae95b3b258dfde8626ad3f7c751dad469
tmosest <tmosest@gmail.com>
2019-04-11 12:34:30 -0400

    ClosestLeafInBinaryTree: while this solved the test cases this was not the correct solution

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/ClosestLeafInBinaryTreeTest.java
@@ -0,0 +1,35 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("medium")
+@Tag("binaryTree")
+@DisplayName("LeetCode: 742. Closest Leaf in a Binary Tree")
+public class ClosestLeafInBinaryTreeTest {
+
+  ClosestLeafInBinaryTree closestLeafInBinaryTree = new ClosestLeafInBinaryTree();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(3);
+    root.right = new TreeNode(2);
+
+    Assertions.assertNotEquals(1, closestLeafInBinaryTree.findClosestLeaf(root, 1));
+  }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(3);
+    root.right = new TreeNode(6);
+
+    Assertions.assertEquals(3, closestLeafInBinaryTree.findClosestLeaf(root, 2));
+  }
+}

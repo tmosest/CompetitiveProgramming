95c1e3217f6ad8c62ee338a96967e9f6acc7a0b6
tmosest <tmosest@gmail.com>
2019-04-11 21:03:54 -0400

    refactor some leet code exercises

---
+++ src/main/java/com/tmosest/competitiveprogramming/utils/UtilGraph.java
@@ -0,0 +1,54 @@
+package com.tmosest.competitiveprogramming.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UtilGraph<T> {
+  // The nodes in the utilGraph.
+  public List<UtilGraphNode<T>> nodes = new ArrayList<>();
+
+  /**
+   * Create a UtilGraph from a tree.
+   * @param root The root of the tree.
+   */
+  public UtilGraph(UtilTreeNode<T> root) {
+    createFromTree(root, null);
+  }
+
+  private void createFromTree(UtilTreeNode<T> root, UtilGraphNode<T> parent) {
+    if (root == null) {
+      return;
+    }
+    UtilGraphNode<T> node = new UtilGraphNode<>(nodes.size(), root.val);
+    if (parent != null) {
+      node.neighbors.add(parent);
+      parent.neighbors.add(node);
+    }
+    nodes.add(node);
+    createFromTree(root.left, node);
+    createFromTree(root.right, node);
+  }
+
+  /**
+   * Create a UtilBreathFirstSearch iterator class from the string point of the utilGraph.
+   * @param start The starting point for Breath First Search.
+   * @return An iteration of bfs over a graph.
+   */
+  public UtilBreathFirstSearch<T> breathFirstSearch(UtilGraphNode<T> start) {
+    return new UtilBreathFirstSearch<>(this, start);
+  }
+
+  /**
+   * Find the first node with the value.
+   * @param val The value of the node we are searching for.
+   * @return A node of the utilGraph with the search value or null.
+   */
+  public UtilGraphNode<T> find(T val) {
+    for (UtilGraphNode<T> node : nodes) {
+      if (node.val == val) {
+        return node;
+      }
+    }
+    return null;
+  }
+}
---
+++ src/main/java/com/tmosest/competitiveprogramming/utils/UtilTreeNode.java
@@ -0,0 +1,44 @@
+package com.tmosest.competitiveprogramming.utils;
+
+/**
+ * A more universal TreeNode that can be used over and over again.
+ * @param <T> Type of TreeNode.
+ */
+public class UtilTreeNode<T> {
+  public T val;
+  public UtilTreeNode<T> left;
+  public UtilTreeNode<T> right;
+
+  public UtilTreeNode(T val) {
+    this.val = val;
+  }
+
+  /**
+   * Determines if a node is a leaf node.
+   * @return are the left and right children null.
+   */
+  public boolean isLeaf() {
+    return left == null && right == null;
+  }
+
+  /**
+   * Find the closest leaf in the path of the node value.
+   * @param search The node value to start searching from.
+   * @return The value of the closest leaf's value.
+   */
+  public T findClosestLeaf(T search) {
+    // Create a utilGraph from the leaf node.
+    UtilGraph<T> utilGraph = new UtilGraph<>(this);
+    UtilGraphNode<T> searchNode = utilGraph.find(search);
+    // Iterator to make it easy to go over the utilGraph with BFS.
+    UtilBreathFirstSearch<T> utilBreathFirstSearch =
+        new UtilBreathFirstSearch<>(utilGraph, searchNode);
+    // Now we can for-each loop over the iterator!
+    for (UtilGraphNode<T> node : utilBreathFirstSearch) {
+      if (node.isLeaf()) {
+        return node.val;
+      }
+    }
+    return null;
+  }
+}

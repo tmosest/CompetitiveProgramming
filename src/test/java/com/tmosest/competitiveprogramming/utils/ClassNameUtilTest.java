51bdf101801e14bc9503ae88247c13427caf56a3
tmosest <tmosest@gmail.com>
2019-04-26 13:12:06 -0400

    created a util class for creating class names and test class names

---
+++ src/test/java/com/tmosest/competitiveprogramming/utils/ClassNameUtilTest.java
@@ -0,0 +1,25 @@
+package com.tmosest.competitiveprogramming.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("Util")
+@DisplayName("Class Name Util Test")
+class ClassNameUtilTest {
+
+  private ClassNameUtil classNameUtil = ClassNameUtil.instance();
+
+  @Test
+  @DisplayName("Should remove spaces and capitalize names")
+  void should_remove_spaces() {
+    Assertions.assertEquals("RabbitsInForest", classNameUtil.convertToClassName("Rabbits in Forest"));
+  }
+
+  @Test
+  @DisplayName("Should be able to create a test class name")
+  void should_append_test() {
+    Assertions.assertEquals("RabbitsInForestTest", classNameUtil.createTestClassName("Rabbits in Forest"));
+  }
+}

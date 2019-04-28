adde46f48cbd9a16a969a8b6939da73127184d3b
tmosest <tmosest@gmail.com>
2019-04-28 16:10:57 -0400

    UniqueSubstringsInWraparoundString: solved

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/UniqueSubstringsInWraparoundString.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/UniqueSubstringsInWraparoundString.java
@@ -1,29 +1,37 @@
 package com.tmosest.competitiveprogramming.leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 class UniqueSubstringsInWraparoundString {
 
   /**
-   * Determine number of words in wrap around of "...zabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd...".
+   * Determine number of words in wrap around of
+   * "...zabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd...".
    * @param word to look for letters in the above string.
    * @return The number of unique occurances in the substring.
    */
   int findSubstringInWraproundString(String word) {
-    char[] letters = word.toCharArray();
-    Set<String> usedWords = new HashSet<>();
-    for (int i = 0; i < letters.length; i++) {
-      StringBuilder stringBuilder = new StringBuilder();
-      char letter = letters[i];
-      stringBuilder.append(letter);
-      usedWords.add(stringBuilder.toString());
-      for (int j = i + 1; j < letters.length && (letters[j] == letter + 1 || letter == 'z' && letters[j] == 'a'); j++) {
-        letter = letters[j];
-        stringBuilder.append(letter);
-        usedWords.add(stringBuilder.toString());
+    int[] subStringsStartingWith = new int[26];
+    int indexOne = 0;
+    int indexTwo = 1;
+    while (indexTwo < word.length()) {
+      while (indexTwo < word.length()
+          && ((word.charAt(indexTwo - 1) == 'z' && word.charAt(indexTwo) == 'a')
+          || word.charAt(indexTwo) - word.charAt(indexTwo - 1) == 1)) {
+        indexTwo++;
       }
+      while (indexOne < indexTwo) {
+        subStringsStartingWith[word.charAt(indexOne) - 'a'] =
+            Math.max(subStringsStartingWith[word.charAt(indexOne) - 'a'], indexTwo - indexOne);
+        indexOne++;
+      }
+      indexTwo++;
     }
-    return usedWords.size();
+    while (indexOne < word.length()) {
+      subStringsStartingWith[word.charAt(indexOne) - 'a'] =
+          Math.max(subStringsStartingWith[word.charAt(indexOne) - 'a'], word.length() - indexOne);
+      indexOne++;
+    }
+    return Arrays.stream(subStringsStartingWith).sum();
   }
 }

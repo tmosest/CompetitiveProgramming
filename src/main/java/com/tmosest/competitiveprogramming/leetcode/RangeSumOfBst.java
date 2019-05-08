1ff3db1bafa8548486a3b39cd80bab8a1e8ea733
tmosest <tmosest@gmail.com>
2019-05-08 16:51:47 -0700

    RangeSumOfBst: solved

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/RangeSumOfBst.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/RangeSumOfBst.java
@@ -2,6 +2,8 @@ package com.tmosest.competitiveprogramming.leetcode;
 
 class RangeSumOfBst {
 
+  private static int sum = 0;
+
   /**
    * Finds the sum of numbers with in a range.
    *
@@ -10,7 +12,21 @@ class RangeSumOfBst {
    * @param right The right most value we are looking for.
    * @return The sum of all nodes with values in [left, right].
    */
-  public int rangeSumBst(TreeNode root, int left, int right) {
-    return 0;
+  int rangeSumBst(TreeNode root, int left, int right) {
+    sum = 0;
+    rangeSumBstHelper(root, left, right);
+    return sum;
+  }
+
+  private void rangeSumBstHelper(TreeNode root, int left, int right) {
+    if (root == null) {
+      return;
+    }
+    int val = root.val;
+    if (left <= val && val <= right) {
+      sum += val;
+    }
+    rangeSumBstHelper(root.left, left, right);
+    rangeSumBstHelper(root.right, left, right);
   }
 }
--- src/test/java/com/tmosest/competitiveprogramming/leetcode/RangeSumOfBstTest.java
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/RangeSumOfBstTest.java
@@ -17,4 +17,17 @@ class RangeSumOfBstTest {
   void testCase0() {
     Assertions.assertEquals(0, rangeSumOfBst.rangeSumBst(null, 0, 0));
   }
+
+  @Test
+  @DisplayName("Test Case 1")
+  void testCase1() {
+    TreeNode treeNode = new TreeNode(10);
+    treeNode.left = new TreeNode(5);
+    treeNode.right = new TreeNode(15);
+    treeNode.left.left = new TreeNode(3);
+    treeNode.left.right = new TreeNode(7);
+    treeNode.right.right = new TreeNode(18);
+
+    Assertions.assertEquals(32, rangeSumOfBst.rangeSumBst(treeNode,7, 15));
+  }
 }

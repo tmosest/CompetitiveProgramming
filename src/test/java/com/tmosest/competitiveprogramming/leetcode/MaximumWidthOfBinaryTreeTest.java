ce2628deb2e748005f541e879992da3a5fa3238b
tmosest <tmosest@gmail.com>
2019-05-08 19:53:50 -0700

    MaximumWidthOfBinaryTree: solved

---
+++ src/test/java/com/tmosest/competitiveprogramming/leetcode/MaximumWidthOfBinaryTreeTest.java
@@ -0,0 +1,28 @@
+package com.tmosest.competitiveprogramming.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("leetcode")
+@Tag("medium")
+@Tag("binaryTree")
+@DisplayName("LeetCode: 662. Maximum Width of Binary Tree")
+class MaximumWidthOfBinaryTreeTest {
+
+  private MaximumWidthOfBinaryTree maximumWidthOfBinaryTree = new MaximumWidthOfBinaryTree();
+
+  @Test
+  @DisplayName("Test Case 0")
+  void testCase0() {
+    TreeNode root = new TreeNode(1);
+    root.right = new TreeNode(2);
+    root.left = new TreeNode(3);
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(3);
+    root.right.right = new TreeNode(9);
+
+    Assertions.assertEquals(4, maximumWidthOfBinaryTree.widthOfBinaryTree(root));
+  }
+}

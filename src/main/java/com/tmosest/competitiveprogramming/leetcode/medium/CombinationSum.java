866040169558a865a54cc62416df6d278c417492
tmosest <tmosest@gmail.com>
2019-05-24 13:42:54 -0700

    CombinationSum: refactor with CombinationSumTwo

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/CombinationSum.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/CombinationSum.java
@@ -1,6 +1,7 @@
 package com.tmosest.competitiveprogramming.leetcode.medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,18 +9,38 @@ class CombinationSum {
 
   /**
    * Determine all the unique ways to sum to a target.
-   * @param candidates The values we are allowed to sum.
-   * @param target The target value we are trying to sum to.
-   * @return A list of all the list sums.
+   * @param candidates The candidates to sum with.
+   * @param target The target to sum to.
+   * @param dupeCandidatesAllowed Are dupes of the candidates allowed.
+   * @return List of list sums.
    */
-  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+  public List<List<Integer>> combinationSum(
+      int[] candidates,
+      int target,
+      boolean dupeCandidatesAllowed
+  ) {
+    Arrays.sort(candidates);
     List<List<Integer>> result = new ArrayList<>();
     for (int i = candidates.length - 1; i > -1; i--) {
       int remainder = target - candidates[i];
       if (remainder < 0) {
         continue;
       }
-      List<List<Integer>> remainders = combinationSum(candidates, remainder);
+      int[] remainingCandidates = candidates;
+      if (!dupeCandidatesAllowed) {
+        remainingCandidates = new int[candidates.length - 1];
+        int index = 0;
+        for (int j = 0; j < candidates.length; j++) {
+          if (i != j) {
+            remainingCandidates[index++] = candidates[j];
+          }
+        }
+      }
+      List<List<Integer>> remainders = combinationSum(
+          remainingCandidates,
+          remainder,
+          dupeCandidatesAllowed
+      );
       if (remainder == 0 && remainders.size() == 0) {
         remainders.add(new ArrayList<>());
       }
@@ -32,5 +53,15 @@ class CombinationSum {
       }
     }
     return result;
+  }
+
+  /**
+   * Determine all the unique ways to sum to a target.
+   * @param candidates The values we are allowed to sum.
+   * @param target The target value we are trying to sum to.
+   * @return A list of all the list sums.
+   */
+  List<List<Integer>> combinationSum(int[] candidates, int target) {
+    return combinationSum(candidates, target, true);
   }
 }
--- src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/CombinationSumTwo.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/CombinationSumTwo.java
@@ -1,11 +1,10 @@
 package com.tmosest.competitiveprogramming.leetcode.medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 class CombinationSumTwo {
+
+  private CombinationSum combinationSum = new CombinationSum();
 
   /**
    * Same as CombinationSum but not allowed to duplicate candidates.
@@ -14,32 +13,6 @@ class CombinationSumTwo {
    * @return A list of lists which are the individual sums.
    */
   public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-    Arrays.sort(candidates);
-    List<List<Integer>> result = new ArrayList<>();
-    for (int i = candidates.length - 1; i > -1; i--) {
-      int remainder = target - candidates[i];
-      if (remainder < 0) {
-        continue;
-      }
-      int[] remainingCandidates = new int[candidates.length - 1];
-      int index = 0;
-      for (int j = 0; j < candidates.length; j++) {
-        if (i != j) {
-          remainingCandidates[index++] = candidates[j];
-        }
-      }
-      List<List<Integer>> remainders = combinationSum2(remainingCandidates, remainder);
-      if (remainder == 0 && remainders.size() == 0) {
-        remainders.add(new ArrayList<>());
-      }
-      for (List<Integer> list : remainders) {
-        list.add(candidates[i]);
-        Collections.sort(list);
-        if (!result.contains(list)) {
-          result.add(list);
-        }
-      }
-    }
-    return result;
+    return combinationSum.combinationSum(candidates, target, false);
   }
 }

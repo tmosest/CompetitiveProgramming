7f3b347338ea7e41a5dc646fba407c1cd75bd859
tmosest <tmosest@gmail.com>
2019-06-04 14:50:14 -0700

    GreatestCommonDivisorOfStrings: solved

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/LeetCodeGenerator.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/LeetCodeGenerator.java
@@ -61,9 +61,9 @@ public class LeetCodeGenerator {
    * @param args Command line.... not needed.
    */
   public static void main(String[] args) {
-    String name = "1064. Fixed Point";
+    String name = "1071. Greatest Common Divisor of Strings";
     String difficulty = "easy";
-    String functionDeclaration = "public int fixedPoint(int[] arr)";
+    String functionDeclaration = "public String gcdOfStrings(String str1, String str2)";
     try {
       instance().createNewProblem(name, functionDeclaration, difficulty);
       System.out.println(name + " created.");

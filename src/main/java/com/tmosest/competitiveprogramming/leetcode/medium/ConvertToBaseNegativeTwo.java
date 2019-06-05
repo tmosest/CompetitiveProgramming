4f06f8afe88f3313883a6c6cb364c3ec73589e27
tmosest <tmosest@gmail.com>
2019-06-05 16:01:18 -0700

    ConvertToBaseNegativeTwo: solved

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/ConvertToBaseNegativeTwo.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/ConvertToBaseNegativeTwo.java
@@ -1,5 +1,18 @@
 package com.tmosest.competitiveprogramming.leetcode.medium;
 
+/**
+ * 0 -> 0.
+ * 1 -> 1.
+ * 2 -> 110 (4 - 2 + 0).
+ * 3 -> 111.
+ * 4 -> 100.
+ * 5 -> 101.
+ * 6 -> 11010.
+ * 7 -> 11011.
+ * 8 -> 11000.
+ * 9 -> 11001.
+ * 10 -> 11111.
+ */
 class ConvertToBaseNegativeTwo {
 
   /**
@@ -9,6 +22,6 @@ class ConvertToBaseNegativeTwo {
    * @return The negative base two string of it.
    */
   public String baseNeg2(int num) {
-    return "110";
+    return Integer.toBinaryString(1431655765 ^ (1431655765 - num));
   }
 }

6d37ed8ad6706ad9b24a3851f9073b78a04f9330
tmosest <tmosest@gmail.com>
2019-06-11 17:32:55 -0700

    BinaryTreeZigzagLevelOrderTraversal: refactored

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,13 +1,14 @@
 package com.tmosest.competitiveprogramming.leetcode.medium;
 
 import com.tmosest.competitiveprogramming.leetcode.common.TreeNode;
+import com.tmosest.competitiveprogramming.leetcode.common.TreeNodeAdapter;
+import com.tmosest.competitiveprogramming.utils.tree.UtilTreeNode;
 
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 class BinaryTreeZigzagLevelOrderTraversal {
-  /* Write code here. */
 
   /**
    * Level by level traversal with zigzagging.
@@ -15,39 +16,21 @@ class BinaryTreeZigzagLevelOrderTraversal {
    * @param root The root of the tree.
    * @return A list of list of zigzagged levels.
    */
-  public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+  List<List<Integer>> zigzagLevelOrder(TreeNode root) {
     List<List<Integer>> result = new LinkedList<>();
     if (root == null) {
       return result;
     }
-    LinkedList<TreeNode> queue = new LinkedList<>();
-    queue.add(root);
-    boolean forwards = true;
-    while (!queue.isEmpty()) {
-      int size = queue.size();
-      LinkedList<TreeNode> nextQueue = new LinkedList<>();
-      List<Integer> row = new LinkedList<>();
-      for (int i = 0; i < size; i++) {
-        TreeNode node = queue.poll();
-        if (node != null) {
-          row.add(node.val);
-          if (node.left != null) {
-            nextQueue.add(node.left);
-          }
-          if (node.right != null) {
-            nextQueue.add(node.right);
-          }
-        }
-      }
-      if (!forwards) {
+    // Convert to generic TreeNode.
+    UtilTreeNode<Integer> utilTreeRoot = TreeNodeAdapter.convertToUtility(root);
+    // Iterate over values level by level.
+    for (List<Integer> row : utilTreeRoot.iterateLevelByLevelValuesOnly()) {
+      // Every other row, we reverse the row.
+      if (result.size() == 1) {
         Collections.reverse(row);
       }
       result.add(row);
-      queue = nextQueue;
-      forwards = !forwards;
     }
     return result;
   }
-
-
 }

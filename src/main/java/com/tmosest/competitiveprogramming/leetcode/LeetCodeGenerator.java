6cfaa29d7cec984d127b884ea53e14f3dc2000f4
tmosest <tmosest@gmail.com>
2019-07-23 20:23:25 -0700

    PrintPalindrom: solved

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/LeetCodeGenerator.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/LeetCodeGenerator.java
@@ -61,9 +61,9 @@ public class LeetCodeGenerator {
    * @param args Command line.... not needed.
    */
   public static void main(String[] args) {
-    String name = "939. Minimum Area Rectangle\n";
+    String name = "866. Prime Palindrome\n";
     String difficulty = "medium";
-    String functionDeclaration = "public int minAreaRect(int[][] points)";
+    String functionDeclaration = "public int primePalindrome(int N)";
     try {
       instance().createNewProblem(name, functionDeclaration, difficulty);
       System.out.println(name + " created.");

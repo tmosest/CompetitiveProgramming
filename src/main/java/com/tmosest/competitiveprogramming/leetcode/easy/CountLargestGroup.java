1427daa405295dadfa954e64f35df71b94e135e3
tmosest <tmosest@gmail.com>
2020-04-05 17:32:42 -0700

    CountLargestGroup: solved

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/easy/CountLargestGroup.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/easy/CountLargestGroup.java
@@ -7,38 +7,38 @@ import java.util.Map;
 
 class CountLargestGroup {
 
-	int countLargestGroup(int num) {
-		Map<Integer, List<Integer>> digitSumMap = new HashMap<>();
+  int countLargestGroup(int num) {
+    Map<Integer, List<Integer>> digitSumMap = new HashMap<>();
 
-		int maxSize = 0;
+    int maxSize = 0;
 
-		for (int i = 1; i <= num; i++) {
-			int sum = sumDigits(i);
-			List<Integer> sums = digitSumMap.getOrDefault(sum, new ArrayList<>());
-			sums.add(i);
-			digitSumMap.put(sum, sums);
-			maxSize = Math.max(maxSize, sums.size());
-		}
+    for (int i = 1; i <= num; i++) {
+      int sum = sumDigits(i);
+      List<Integer> sums = digitSumMap.getOrDefault(sum, new ArrayList<>());
+      sums.add(i);
+      digitSumMap.put(sum, sums);
+      maxSize = Math.max(maxSize, sums.size());
+    }
 
-		int res = 0;
+    int res = 0;
 
-		for (List<Integer> list : digitSumMap.values()) {
-			if (list.size() == maxSize) {
-				++res;
-			}
-		}
+    for (List<Integer> list : digitSumMap.values()) {
+      if (list.size() == maxSize) {
+        ++res;
+      }
+    }
 
-		return res;
-	}
+    return res;
+  }
 
-	private int sumDigits(int num) {
-		int res = 0;
+  private int sumDigits(int num) {
+    int res = 0;
 
-		while (num > 0) {
-			res += num % 10;
-			num /= 10;
-		}
+    while (num > 0) {
+      res += num % 10;
+      num /= 10;
+    }
 
-		return res;
-	}
+    return res;
+  }
 }

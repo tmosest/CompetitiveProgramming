59641e52110200f513d7bed497e60fa856842d95
tmosest <tmosest@gmail.com>
2020-04-29 18:47:01 -0700

    FirstUnique: actual solution

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/FirstUnique.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/FirstUnique.java
@@ -2,17 +2,21 @@ package com.tmosest.competitiveprogramming.leetcode.medium;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 class FirstUnique {
 
-	private List<Integer> list;
 	private Map<Integer, Integer> counts;
+	private List<Integer> uniqueQueue;
+	private Set<Integer> uniqueValues;
 
 	FirstUnique(int[] nums) {
-		list = new ArrayList<>();
 		counts = new HashMap<>();
+		uniqueQueue = new ArrayList<>();
+		uniqueValues = new HashSet<>();
 
 		for (int num : nums) {
 			this.add(num);
@@ -20,18 +24,25 @@ class FirstUnique {
 	}
 
 	int showFirstUnique() {
-		for (int value : list) {
-			int count = counts.getOrDefault(value, 0);
-			if (count == 1) {
-				return value;
-			}
+		if (uniqueQueue.size() > 0) {
+			return uniqueQueue.get(0);
 		}
+
 		return -1;
 	}
 
 	void add(int value) {
-		list.add(value);
 		int count = counts.getOrDefault(value, 0);
-		counts.put(value, count + 1);
+		count += 1;
+		counts.put(value, count);
+
+		if (count == 1) {
+			uniqueQueue.add(value);
+			uniqueValues.add(value);
+		} else if (uniqueValues.contains(value)) {
+			Integer val = Integer.valueOf(value);
+			uniqueQueue.remove(val);
+			uniqueValues.remove(val);
+		}
 	}
 }

5aa97b8aa5e7675ff3c95a10ae844dfe7977bbb2
tmosest <tmosest@gmail.com>
2020-07-19 20:42:28 -0400

    LeetCode: 1520. Maximum Number of Non-Overlapping Substrings: solved

--- src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/MaximumNumberOfNonoverlappingSubstrings.java
+++ src/main/java/com/tmosest/competitiveprogramming/leetcode/medium/MaximumNumberOfNonoverlappingSubstrings.java
@@ -14,21 +14,24 @@ class MaximumNumberOfNonoverlappingSubstrings {
     HashMap<Character, int[]> range = new HashMap<>();
 
     // get each character's range
-    for(int i = 0; i < s.length; i++) {
+    for (int i = 0; i < s.length; i++) {
       char cur = s[i];
-      if(range.containsKey(cur)) range.get(cur)[1] = i;
-      else range.put(cur, new int[]{i, i});
+      if (range.containsKey(cur)) {
+        range.get(cur)[1] = i;
+      } else {
+        range.put(cur, new int[]{i, i});
+      }
     }
 
     // extend the range
     List<int[]> values = new ArrayList<>(range.values());
-    for(int[] r : values) {
+    for (int[] r : values) {
 
       // gather all characters in the range r[0]...r[1]
       Set<Character> seen = new HashSet<>();
       int size = 0, min = r[0], max = r[1];
-      for(int i = r[0]; i <= r[1]; i++) {
-        if(seen.add(s[i])) {
+      for (int i = r[0]; i <= r[1]; i++) {
+        if (seen.add(s[i])) {
           int temp[] = range.get(s[i]);
           min = Math.min(min, temp[0]);
           max = Math.max(max, temp[1]);
@@ -36,21 +39,21 @@ class MaximumNumberOfNonoverlappingSubstrings {
       }
 
       // extend the range untile the range will not cover any other new character
-      while(seen.size() != size) {
+      while (seen.size() != size) {
         size = seen.size();
         int nMin = min;
         int nMax = max;
 
-        for(int i = min; i < r[0]; i++) {
-          if(seen.add(s[i])){
+        for (int i = min; i < r[0]; i++) {
+          if (seen.add(s[i])) {
             int temp[] = range.get(s[i]);
             nMin = Math.min(nMin, temp[0]);
             nMax = Math.max(nMax, temp[1]);
           }
         }
 
-        for(int i = r[1] + 1; i <= max; i++) {
-          if(seen.add(s[i])){
+        for (int i = r[1] + 1; i <= max; i++) {
+          if (seen.add(s[i])) {
             int temp[] = range.get(s[i]);
             nMin = Math.min(nMin, temp[0]);
             nMax = Math.max(nMax, temp[1]);
@@ -65,14 +68,14 @@ class MaximumNumberOfNonoverlappingSubstrings {
       }
     }
 
-    Collections.sort(values, (a, b) -> a[1] - b[1]);
+    Collections.sort(values, (one, two) -> one[1] - two[1]);
 
-    int p = -1;
+    int pointers = -1;
     List<String> res = new ArrayList<>();
 
-    for(int[] r : values) {
-      if(r[0] > p) { // can be accepted
-        p = r[1]; // p means lastest used index
+    for (int[] r : values) {
+      if (r[0] > pointers) { // can be accepted
+        pointers = r[1]; // p means lastest used index
         res.add(ss.substring(r[0], r[1] + 1));
       }
     }
